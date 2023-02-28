89e5820273ebb328fcbbf13dedd5278b1b77e630
dmi <dmi.villione@gmail.com>
2023-02-28 17:43:25 +0300

    add vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,52 @@ import react from '@vitejs/plugin-react'
 const packageJson = require('./package.json')
 import { defineConfig } from 'vite'
 import checker from 'vite-plugin-checker'
+import git from 'git-rev-sync'
+import rollupNodePolyfills from 'rollup-plugin-node-polyfills'
+
+let commit = git.short()
+
+const version = packageJson.version
+console.log(`Building app v${version} (${commit})`)
 
 export default defineConfig({
-    base: '',
-    define: {
-        VITE_PACKAGE_VERSION: JSON.stringify(packageJson.version),
-        VITE_PACKAGE_NAME: JSON.stringify(packageJson.name),
-    },
-    build: {
-        assetsDir: '',
-    },
-    plugins: [
-        react({ fastRefresh: false }),
-        checker({
-            typescript: true,
-        }),
-    ],
+	base: '',
+	define: {
+		'process.env': JSON.stringify({
+			VITE_APP_VERSION: version,
+			VITE_GIT_COMMIT: commit,
+		}),
+	},
+	build: {
+		assetsDir: '',
+	},
+	plugins: [
+		react({ fastRefresh: false }),
+		checker({
+			typescript: true,
+		}),
+	],
+	resolve: {
+		alias: [
+			...[/* "events", */ /* "path", */ /* "util", */ 'url', 'http'].map(
+				(x) => ({
+					find: x,
+					replacement: `rollup-plugin-node-polyfills/polyfills/${x}`,
+				})
+			),
+			// { find: 'child_process', replacement: 'rollup-plugin-node-polyfills' },
+			{
+				find: 'https',
+				replacement: 'rollup-plugin-node-polyfills/polyfills/http',
+			},
+			{
+				find: 'querystring',
+				replacement: 'rollup-plugin-node-polyfills/polyfills/qs',
+			},
+			{ find: 'process', replacement: 'process/browser' },
+			{ find: 'stream', replacement: 'stream-browserify' },
+			{ find: 'zlib', replacement: 'browserify-zlib' },
+			{ find: 'util', replacement: 'util/util' },
+		],
+	},
 })
