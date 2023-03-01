f558288fe74552bd0982d267aefa4a28c059503e
dmi <dmi.villione@gmail.com>
2023-03-01 16:56:35 +0300

    add join type

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,14 @@
 export type Remap<Obj, R, W> = {
 	[K in keyof Obj]: Obj[K] extends R ? W : R
 }
+
+export type Join<
+	S extends string[],
+	Separator extends string = ''
+> = S extends [string]
+	? S[0]
+	: S extends [string, ...infer Tail]
+	? Tail extends string[]
+		? `${S[0]}${Separator}${Join<Tail, Separator>}`
+		: `${S[0]}`
+	: never
