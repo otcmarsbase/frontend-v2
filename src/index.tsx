e2a78af8e402041648ce569cf5426242a48a9c7f
dmi <dmi.villione@gmail.com>
2023-03-01 11:37:53 +0300

    add swr

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from './utils/chakra'
 import { App } from './App'
+import { SWRConfig } from 'swr'
 
 const container = document.getElementById('root')
 const root = createRoot(container!)
 
 root.render(
 	<ChakraProvider resetCSS={false} theme={theme}>
-		<App />
+		<SWRConfig value={{ provider: () => new Map() }}>
+			<App />
+		</SWRConfig>
 	</ChakraProvider>
 )
