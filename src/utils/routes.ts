944d5f8559b97452cff06e2bcbb24d190ed42ae4
dmi <dmi.villione@gmail.com>
2023-03-01 17:00:37 +0300

    add routeWithParam

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -6,7 +6,7 @@ type SwapParam<Key extends string, T extends `${string}/${Key}${string}`, Value 
 	: T extends `/${Key}${infer Suffix}` ? `${Value}${Suffix}`
 	: T extends `/${Key}` ? `${Value}`
 	: never
-    
+
 export type RemapBranch<Branch, Prefix extends string> = {
 	[K in keyof Branch]:
 		Branch extends { _: infer Prefix2 extends string }
@@ -17,7 +17,16 @@ export type RemapBranch<Branch, Prefix extends string> = {
 		: never
 }
 
-
 export type Remap<Obj, R, W> = {
 	[K in keyof Obj]: Obj[K] extends R ? W : R
+}
+
+
+export function routeWithParam<
+	Key extends `:${string}`,
+	Route extends `${string}/${Key}${string}`,
+	Id extends string | number
+>(route: Route, key: Key, id: Id): SwapParam<Key, Route, Id>
+{
+	return route.replace(key, id + '') as any
 }
