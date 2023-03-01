8c50e69a86595f93e30cbc8db9ce09c25b68afae
dmi <dmi.villione@gmail.com>
2023-03-01 16:59:02 +0300

    add RemapBranch type

---
+++ src/utils/routes.ts
@@ -0,0 +1,11 @@
+import { Join } from "../types";
+
+export type RemapBranch<Branch, Prefix extends string> = {
+	[K in keyof Branch]:
+		Branch extends { _: infer Prefix2 extends string }
+			? Branch[K] extends string
+			? K extends "_" ? { short: Branch[K], full: Join<[Prefix, Branch[K]]> }
+			: { short: Branch[K], full: Join<[Prefix, Prefix2, Branch[K]]> }
+			: RemapBranch<Branch[K], Join<[Prefix, Prefix2]>>
+		: never
+}
