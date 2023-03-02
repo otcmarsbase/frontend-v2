6fd44854cbc170492424871066de75d31a10afe1
dmi <dmi.villione@gmail.com>
2023-03-02 17:56:09 +0300

    add calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { Calculator } from './pages/Calculator'
 import { ROUTES } from './utils/routes'
 
 type AppProps = {}
@@ -9,6 +10,7 @@ export const App: React.FC<AppProps> = ({}) => {
 		<BrowserRouter>
 			<Routes>
                 <Route path={ROUTES.offerDetails} element={<div></div>} />
+                <Route path={ROUTES.calculator} element={<Calculator />} />
             </Routes>
 		</BrowserRouter>
 	)
