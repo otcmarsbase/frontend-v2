41ae32326a4cb59f33b2acb1094fef3ebb94b6ff
dmi <dmi.villione@gmail.com>
2023-03-02 17:55:56 +0300

    add offerDetail

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { ROUTES } from './utils/routes'
 
 type AppProps = {}
 
 export const App: React.FC<AppProps> = ({}) => {
 	return (
 		<BrowserRouter>
-			<div>Hello, world!</div>
+			<Routes>
+                <Route path={ROUTES.offerDetails} element={<div></div>} />
+            </Routes>
 		</BrowserRouter>
 	)
 }
