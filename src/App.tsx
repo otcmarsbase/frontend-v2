adaf2b6eada9427a2f11bd627e1b78bbde2f01a1
dmi <dmi.villione@gmail.com>
2023-03-02 18:07:25 +0300

    remove comment

--- src/App.tsx
+++ src/App.tsx
@@ -41,19 +41,6 @@ export const App: React.FC<AppProps> = ({}) => {
 							path={ROUTES.bestbid.offer.view.full}
 							element={<BBViewOfferRoute creatingBid={false} />}
 						/>
-						{/* <Route path={ROUTES.bestbid.offer.success.full} element={<BBOfferSuccess />} /> */}
-						<Route
-							path={ROUTES.bestbid.offer.bid_create.full}
-							element={<BBViewOfferRoute creatingBid={true} />}
-						/>
-						<Route path={ROUTES.bestbid.offer.bid._.full}>
-							<Route
-								path={ROUTES.bestbid.offer.bid.view.full}
-								element={
-									<BBViewOfferRoute creatingBid={false} />
-								}
-							/>
-						</Route>
 					</Route>
 				</Route>
 			</Routes>
