e1bb01c21e7cd6cce09b675c608b800a7242fde8
dmi <dmi.villione@gmail.com>
2023-03-03 17:36:12 +0300

    rename map type

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -18,6 +18,6 @@ type ParseParams<K, Result> =
 type ExtractParams<K> = ParseParams<K, {}>
 type Tree = { path: string; children?: Tree[] }
 
-type Map<A extends Tree[], Prefix extends string> = {
+type MapHeadToStackFrame<A extends Tree[], Prefix extends string> = {
 	[K in keyof A]: { prefix: Prefix; frame: A[K] }
 }
