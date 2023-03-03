050de6feba44d3aa559c30425b80ace96be5e2d9
dmi <dmi.villione@gmail.com>
2023-03-03 17:58:46 +0300

    add AppRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Route, Navigate, useRoutes } from 'react-router-dom'
 import { Calculator } from './pages/Calculator'
+import { DeepWriteable, flatRoutes } from './utils/routes'
 
 type AppProps = {}
 
-const routes = [{ path: '/calculator', element: <Calculator /> }] as const
+const routes = [
+	{
+		path: '/calculator',
+		element: <Calculator />,
+	},
+] as const
+
 export const App: React.FC<AppProps> = ({}) => {
 	return (
 		<BrowserRouter>
-			
+			<AppRoutes />
 		</BrowserRouter>
 	)
+}
+
+const AppRoutes: React.FC = () => {
+	const Routes = useRoutes(routes as any)
+	const flattenRoutes = flatRoutes(routes as DeepWriteable<typeof routes>)
+
+	return Routes
 }
