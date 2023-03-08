03a9f93b3e58b523dbb2d52d7def3fa24b06bcd1
dmi <dmi.villione@gmail.com>
2023-03-08 11:12:46 +0300

    add offers/waiting

--- src/App.tsx
+++ src/App.tsx
@@ -60,6 +60,7 @@ const routes = [
 				children: [
 					{ path: '', element: <div>bestbid offers root</div> },
 					{ path: 'new', element: <div>bestbid offers new</div> },
+					{ path: 'new/waiting/:txid', element: <div>bestbid offers waiting</div> },
 				],
 			},
 		],
