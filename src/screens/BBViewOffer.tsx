5ba4a6a3eaa5f69d367d91ba9319a68f5ae7f1a2
dmi <dmi.villione@gmail.com>
2023-03-09 14:15:39 +0300

    add todo reminder

--- src/screens/BBViewOffer.tsx
+++ src/screens/BBViewOffer.tsx
@@ -4,6 +4,7 @@ type BBViewOfferProps = {
 	creatingBid: boolean
 }
 
+// TODO: Можно ли разделить экраны так, чтобы не прокидывать creatingBid?
 export const BBViewOffer: React.FC<BBViewOfferProps> = ({ creatingBid }) => {
 	return <div>BBViewOffer screen</div>
 }
