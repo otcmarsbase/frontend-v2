b11ecba7497cc2f5c02b2a9c15a55381d658fa95
dmi <dmi.villione@gmail.com>
2023-03-11 11:13:01 +0300

    add page wrapper

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,22 @@
+import { Container, VStack } from '@chakra-ui/react'
 import React from 'react'
 
 type LayoutProps = {
 	top?: React.ReactNode
 }
 
-export const Layout: React.FCC<LayoutProps> = ({ top }) => {
-	return <div></div>
+export const Layout: React.FCC<LayoutProps> = ({ top, children }) => {
+	return (
+		<PageWrapper>
+			<Container paddingX={'20px'}>{children}</Container>
+		</PageWrapper>
+	)
+}
+
+const PageWrapper: React.FCC = ({ children }) => {
+	return (
+		<VStack minHeight={'100vh'} height={'100%'}>
+			{children}
+		</VStack>
+	)
 }
