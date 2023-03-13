6f7e43eea3031461592183815ffc7e5b2f04b4cb
dmi <dmi.villione@gmail.com>
2023-03-13 11:13:56 +0300

    add footer grid

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,33 @@
+import { Box, Grid, GridItem, HStack } from '@chakra-ui/react'
 import React from 'react'
 
-type FooterProps = {}
+type FooterProps = {
+	title: React.ReactNode
+	description: React.ReactNode
+	socialLinks: React.ReactNode
+	copyRight: React.ReactNode
+	menuLinks: React.ReactNode[]
+}
 
-export const Footer: React.FC<FooterProps> = ({}) => {
-    return <div></div>
+export const Footer: React.FC<FooterProps> = ({
+	copyRight,
+	description,
+	socialLinks,
+	title,
+	menuLinks,
+}) => {
+	return (
+		<HStack width={'100%'} justifyContent={'space-between'}>
+			<Box>
+				<Grid
+					templateColumns="repeat(2, 1fr)"
+					templateRows={'repeat(4, 1fr)'}
+                    gap={2}
+				>
+					{menuLinks}
+				</Grid>
+			</Box>
+			<Box>social</Box>
+		</HStack>
+	)
 }
