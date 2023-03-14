f422e3d4b2e22f14f1e62953c7de392544da18ff
dmi <dmi.villione@gmail.com>
2023-03-14 10:52:54 +0300

    add social links

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -53,18 +53,31 @@ export const links = {
 	},
 	footer: {
 		pitchDesk:
-		  'https://drive.google.com/file/d/1OerpyQdyqwKixW0Mc5D16-vumskeG8qK/view',
+			'https://drive.google.com/file/d/1OerpyQdyqwKixW0Mc5D16-vumskeG8qK/view',
 		license:
-		  'https://drive.google.com/file/d/1nf3_Q3ZOIBAqf5w6JVzekKACsfsIQTkB/view?usp=sharing',
+			'https://drive.google.com/file/d/1nf3_Q3ZOIBAqf5w6JVzekKACsfsIQTkB/view?usp=sharing',
 		privacyPolicy:
-		  'https://drive.google.com/file/d/1mHwB3uSvzpRFOQftK8EOTmxMRX2LVfq8/view?usp=sharing',
+			'https://drive.google.com/file/d/1mHwB3uSvzpRFOQftK8EOTmxMRX2LVfq8/view?usp=sharing',
 		whitePaper:
-		  'https://drive.google.com/file/d/1Sa_eUfxlkOuOYMC9pktU-Nbgp5uKL3A_/view',
+			'https://drive.google.com/file/d/1Sa_eUfxlkOuOYMC9pktU-Nbgp5uKL3A_/view',
 		support: 'https://intercom.help/marsbase/en',
 		dOTCDesk: 'https://app.otcmarsbase.io/en/trading#',
-		terms:
-		  'https://drive.google.com/file/d/1PlJSXdu7b8dH3EAY7XY7NPSsB9YZS6Pg/view?usp=sharing',
+		terms: 'https://drive.google.com/file/d/1PlJSXdu7b8dH3EAY7XY7NPSsB9YZS6Pg/view?usp=sharing',
 		knowledgeBase:
-		  'https://otc-marsbase.gitbook.io/marsbase/D4i0W5CUWz8SJkPxT7zF/',
-	  },
+			'https://otc-marsbase.gitbook.io/marsbase/D4i0W5CUWz8SJkPxT7zF/',
+	},
+	social: {
+		fb: 'https://facebook.com/',
+		twitter: 'https://twitter.com/MARSBASEio',
+		vk: 'https://vk.com/',
+		youtube: 'https://youtube.com/',
+		google: 'https://google.com',
+		ok: 'https://ok.ru',
+		discord:
+			'https://discord.com/channels/889519633890177074/892286634224132106',
+		reddit: 'https://www.reddit.com/r/MarsbaseCryptoOTC/',
+		telegram: 'https://t.me/otcmarsbase',
+		linktree: 'https://linktr.ee/MARSBASE',
+		medium: 'https://marsbaseotc.medium.com/',
+	},
 }
