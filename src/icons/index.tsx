1ae1e6c46cbd332104999d53b85d7a8468f09bc5
dmi <dmi.villione@gmail.com>
2023-03-14 10:52:33 +0300

    add social icons

---
+++ src/icons/index.tsx
@@ -0,0 +1,15 @@
+import DiscordIcon from '../assets/icon/socials/akar-icons_discord-fill.svg'
+import RedditIcon from '../assets/icon/socials/akar-icons_reddit-fill.svg'
+import TelegramIcon from '../assets/icon/socials/bxl_telegram.svg'
+import LinktreeIcon from '../assets/icon/socials/simple-icons_linktree.svg'
+import TwitterIcon from '../assets/icon/socials/akar-icons_twitter-fill.svg'
+import MediumIcon from '../assets/icon/socials/bi_medium.svg'
+
+export {
+	DiscordIcon,
+	RedditIcon,
+	TelegramIcon,
+	LinktreeIcon,
+	TwitterIcon,
+	MediumIcon,
+}
