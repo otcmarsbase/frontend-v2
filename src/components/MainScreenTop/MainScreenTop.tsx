18617e482d0a7578ab0cc6821356903d25ae62d6
dmi <dmi.villione@gmail.com>
2023-03-15 10:53:49 +0300

    add dummy MainScreenTop component

---
+++ src/components/MainScreenTop/MainScreenTop.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type MainScreenTopProps = {
+    onCreateOfferClick: () => void
+}
+
+export const MainScreenTop: React.FC<MainScreenTopProps> = ({ onCreateOfferClick }) => {
+
+
+    return (
+       <div></div>
+    )
+}
