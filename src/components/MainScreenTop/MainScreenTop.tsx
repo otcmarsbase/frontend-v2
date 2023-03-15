faebea2c4c8ab3d800c8b074f7fa23283632864d
dmi <dmi.villione@gmail.com>
2023-03-15 11:16:41 +0300

    add create offer header

--- src/components/MainScreenTop/MainScreenTop.tsx
+++ src/components/MainScreenTop/MainScreenTop.tsx
@@ -1,13 +1,26 @@
+import { Box, Button, HStack, Link, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
 
 type MainScreenTopProps = {
-    onCreateOfferClick: () => void
+	onCreateOfferClick: () => void
 }
 
-export const MainScreenTop: React.FC<MainScreenTopProps> = ({ onCreateOfferClick }) => {
-
-
-    return (
-       <div></div>
-    )
+export const MainScreenTop: React.FC<MainScreenTopProps> = ({
+	onCreateOfferClick,
+}) => {
+	return (
+		<HStack justifyContent={'space-between'} width={'100%'}>
+			<VStack alignItems={'flex-start'}>
+				<HStack>
+					<Text>OTC Desk</Text>
+					<Link>How to use?</Link>
+				</HStack>
+				<Text>
+					Explore available OTC deals, filter them by token, size,
+					type, or create your own offers
+				</Text>
+			</VStack>
+			<Button>Create Offer</Button>
+		</HStack>
+	)
 }
