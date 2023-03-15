e01ab338156c6c3e29e5ec594451d832877ea55b
dmi <dmi.villione@gmail.com>
2023-03-15 12:42:19 +0300

    update Main screen offer header

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,9 +1,28 @@
+import { Link } from '@chakra-ui/react'
 import React from 'react'
-import { MainScreenTop } from '../components/MainScreenTop/MainScreenTop'
+import { CreateOfferHeader } from '../components/CreateOfferHeader/CreateOfferHeader'
 import { MainScreenWrapper } from '../components/MainScreenWrapper/MainScreenWrapper'
+import { useTranslation } from '../localization/l10n'
 
 type MainProps = {}
 
 export const Main: React.FC<MainProps> = ({}) => {
-	return <MainScreenWrapper top={<MainScreenTop onCreateOfferClick={() => {}}/>}>HELLO WORLD</MainScreenWrapper>
+	const l10n = useTranslation()
+	return (
+		<MainScreenWrapper
+			top={
+				<CreateOfferHeader
+					titleLink={<Link>{l10n.OTCDesk.howToUse}</Link>}
+					createOfferBtn={{
+						label: l10n.OTCDesk.createOfferLabel,
+						onClick: () => {},
+					}}
+					subTitle={l10n.OTCDesk.subTitle}
+					title={l10n.OTCDesk.title}
+				/>
+			}
+		>
+			HELLO WORLD
+		</MainScreenWrapper>
+	)
 }
