d987b9df821fd87a6224a4d4ee52aaeb6f4d3e7e
dmi <dmi.villione@gmail.com>
2023-03-16 16:08:50 +0300

    add token ranks

--- src/components/OTCDesk/OTCDeskFilterPanel.tsx
+++ src/components/OTCDesk/OTCDeskFilterPanel.tsx
@@ -1,7 +1,28 @@
+import { Text, VStack } from '@chakra-ui/react'
+import { title } from 'process'
+import Select from 'react-select'
 import React from 'react'
 
-type OTCDeskFilterPanelProps = {}
+type OTCDeskFilterPanelProps = {
+	title: string
+}
 
+const tokenRanks = [
+	{ value: 'High Liquid - Top 20', label: 'High Liquid - Top 20' },
+	{
+		value: 'Middle Liquid - Top 20 - 50',
+		label: 'Middle Liquid - Top 20 - 50',
+	},
+	{
+		value: 'Middle Liquid - Top 20 - 50',
+		label: 'Middle Liquid - Top 20 - 50',
+	},
+]
 export const OTCDeskFilterPanel: React.FC<OTCDeskFilterPanelProps> = ({}) => {
-    return <div></div>
+	return (
+		<VStack>
+			<Text>{title}</Text>
+			<Select options={tokenRanks} />
+		</VStack>
+	)
 }
