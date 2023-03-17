aa06bb1a6d9e1c5bcaef6636ba6505729bb2998c
dmi <dmi.villione@gmail.com>
2023-03-17 14:17:16 +0300

    add tooltip style

--- src/components/WithTooltip/WithTooltip.tsx
+++ src/components/WithTooltip/WithTooltip.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Tooltip } from 'react-tooltip'
 import 'react-tooltip/dist/react-tooltip.css'
+import styles from './Tooltip.module.scss'
 
 type WithTooltipProps = {
 	intoText: string
@@ -13,10 +14,13 @@ export const WithTooltip: React.FCC<WithTooltipProps> = ({
 	const idRef = React.useRef('my-tooltip' + Math.random())
 	return (
 		<>
-			<div data-tooltip-id={idRef.current} data-tooltip-content={intoText}>
+			<div
+				data-tooltip-id={idRef.current}
+				data-tooltip-content={intoText}
+			>
 				{children}
 			</div>
-			<Tooltip id={idRef.current} />
+			<Tooltip id={idRef.current} className={styles.container} />
 		</>
 	)
 }
