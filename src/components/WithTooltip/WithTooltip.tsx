37d32c7eb17abe22c0b78183d0a012eff31fad15
dmi <dmi.villione@gmail.com>
2023-03-17 10:30:26 +0300

    import tooltip styles

--- src/components/WithTooltip/WithTooltip.tsx
+++ src/components/WithTooltip/WithTooltip.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import ReactTooltip from 'react-tooltip'
+import 'react-tooltip/dist/react-tooltip.css'
 
-type WithTooltipProps = {}
 
-export const WithTooltip: React.FC<WithTooltipProps> = ({}) => {
-    return <div></div>
+type WithTooltipProps = {
+	intoText: string
 }
 
+export const WithTooltip: React.FCC<WithTooltipProps> = ({
+	intoText,
+	children,
+}) => {
+	return (
+		<>
+			<div data-tip={intoText} style={{ display: 'inline-block' }}>
+				{children}
+			</div>
+			<ReactTooltip multiline className="react-tooltip" />
+		</>
+	)
+}
