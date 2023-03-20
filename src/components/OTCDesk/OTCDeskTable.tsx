eea95399ed771297af3b546316314f32eac5bb31
dmi <dmi.villione@gmail.com>
2023-03-20 11:40:15 +0300

    import Table

--- src/components/OTCDesk/OTCDeskTable.tsx
+++ src/components/OTCDesk/OTCDeskTable.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Table } from '../Table/Table'
 
 type OTCDeskTableProps = {}
 
 export const OTCDeskTable: React.FC<OTCDeskTableProps> = ({}) => {
-    return <div></div>
+	return (
+		<Table>
+
+        </Table>
+	)
 }
