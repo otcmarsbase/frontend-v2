9abe8d4d1c0299b0801f77dc738c2192dacfce68
dmi <dmi.villione@gmail.com>
2023-03-20 11:39:52 +0300

    add TableBodyItem

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -34,3 +34,11 @@ export const TableBody: React.FCC = ({ children }) => {
 export const TableSortButton: React.FCC = ({ children }) => {
 	return <Th>{children}</Th>
 }
+
+export const TableBodyItem: React.FCC = ({ children }) => {
+	return (
+		<Tr>
+			<Td>{children}</Td>
+		</Tr>
+	)
+}
