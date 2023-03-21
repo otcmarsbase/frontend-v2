7266c722dd70f62c7c0e5e987c3616a2bb229c6e
dmi <dmi.villione@gmail.com>
2023-03-21 15:31:59 +0300

    add dummy Modal

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,10 @@
+import { ModalBody, Modal as ModalWrapper, ModalContent } from '@chakra-ui/react'
+import React from 'react'
+
+type ModalProps = {}
+
+export const Modal: React.FC<ModalProps> = ({}) => {
+    return <div>
+        <ModalContent ></ModalContent>
+    </div>
+}
