48217915f1584e42a96fa7a0c941c1a113266ffe
dmi <dmi.villione@gmail.com>
2023-03-21 15:35:33 +0300

    add modal props

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,10 +1,19 @@
-import { ModalBody, Modal as ModalWrapper, ModalContent } from '@chakra-ui/react'
+import {
+	ModalBody,
+	Modal as ModalWrapper,
+	ModalContent,
+} from '@chakra-ui/react'
 import React from 'react'
 
-type ModalProps = {}
+type ModalProps = {
+	isOpen?: boolean
+	onClose: () => void
+}
 
-export const Modal: React.FC<ModalProps> = ({}) => {
-    return <div>
-        <ModalContent ></ModalContent>
-    </div>
+export const Modal: React.FC<ModalProps> = ({ isOpen = true, onClose }) => {
+	return (
+		<ModalWrapper isOpen={isOpen} onClose={onClose}>
+			<ModalContent></ModalContent>
+		</ModalWrapper>
+	)
 }
