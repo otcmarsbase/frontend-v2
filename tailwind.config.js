30d59b1032a613520388a8508857d14c08b5543d
dmi <dmi.villione@gmail.com>
2023-03-21 16:25:20 +0300

    add zindex 999 to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,6 +36,9 @@ module.exports = {
         backgroundImage: {
             "popup": "linear-gradient(151.47deg,#8A67FF -7.28%,#49D4FF 29.09%,#FE673C 65.78%,#A6498F 107.39%)",
             "overlay": "radial-gradient(50% 50% at 50% 50%,rgba(36,19,43,.8) 34.93%,rgba(19,20,25,.8) 100%)"
+        },
+        zIndex: {
+            999: 999
         }
       },
     },
