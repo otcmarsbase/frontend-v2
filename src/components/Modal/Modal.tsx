bf93cce5fe91cb9f252b7381722b7404ae0a8f06
dmi <dmi.villione@gmail.com>
2023-03-22 21:23:27 +0300

    put zindex to modal container

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -8,15 +8,15 @@ type ModalProps = {
 
 export const Modal: React.FC<ModalProps> = ({ onClose, renderContent }) => {
 	return (
-		<div className="flex justify-center items-center fixed top-0 right-0 left-0 bottom-0">
+		<div className="flex justify-center items-center z-999 fixed top-0 right-0 left-0 bottom-0">
 			<ModalOverlay />
-			<div className="z-999 relative">{renderContent(onClose)}</div>
+			<div className="relative">{renderContent(onClose)}</div>
 		</div>
 	)
 }
 
 export const ModalOverlay: React.FC = ({}) => {
 	return (
-		<div className="bg-overlay absolute top-0 right-0 left-0 bottom-0 z-999 backdrop-blur-lg" />
+		<div className="bg-overlay absolute top-0 right-0 left-0 bottom-0  backdrop-blur-lg" />
 	)
 }
