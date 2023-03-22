747f050fb4f197555f214943f5c2a7fb10f52b85
dmi <dmi.villione@gmail.com>
2023-03-22 21:23:27 +0300

    remove useless prop

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 type PopupProps = {}
 
-export const Popup: React.FCC<PopupProps> = ({ children, onClose }) => {
+export const Popup: React.FCC<PopupProps> = ({ children }) => {
 	return (
 		<Box className="tablet-vh:bg-popup tablet-vh:w-[436px] rounded-2xl bg-black p-[3px]">
 			{children}
