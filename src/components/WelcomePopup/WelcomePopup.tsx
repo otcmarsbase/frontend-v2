2fb39496071000370068338645c79583b78c69a0
dmi <dmi.villione@gmail.com>
2023-03-22 21:23:27 +0300

    add welcome popup styles

--- src/components/WelcomePopup/WelcomePopup.tsx
+++ src/components/WelcomePopup/WelcomePopup.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Popup } from '../Popup/Popup'
 
+type WelcomePopupProps = {
 	onClose: () => void
 }
 
 export const WelcomePopup: React.FC<WelcomePopupProps> = ({ onClose }) => {
 	return (
+		<Popup>
+			<div className="w-full h-full bg-black rounded-2xl">
+				<div>asd</div>
+				<div>asd</div>
+				<div>asd</div>
+				<div>asd</div>
+			</div>
+		</Popup>
+	)
 }
