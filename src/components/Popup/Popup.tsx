794e21e5bd1bd10c58c2a2030fa3479bd24e10cd
dmi <dmi.villione@gmail.com>
2023-03-22 21:23:27 +0300

    [Popup] add className prop

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -1,11 +1,19 @@
-import { Box } from '@chakra-ui/react'
 import React from 'react'
+import { Box } from '@chakra-ui/react'
+import clsx from 'clsx'
 
-type PopupProps = {}
+type PopupProps = {
+	className?: string
+}
 
-export const Popup: React.FCC<PopupProps> = ({ children }) => {
+export const Popup: React.FCC<PopupProps> = ({ children, className }) => {
 	return (
-		<Box className="tablet-vh:bg-popup rounded-2xl bg-black p-[3px]">
+		<Box
+			className={clsx(
+				`tablet-vh:bg-popup tablet-vh:rounded-2xl p-[3px] mx-auto`,
+				className
+			)}
+		>
 			{children}
 		</Box>
 	)
