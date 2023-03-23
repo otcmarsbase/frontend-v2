99e1f5ee37e57d18ea46120c00a39f3884a12d5c
dmi <dmi.villione@gmail.com>
2023-03-23 09:59:32 +0300

    generate icons

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,21 +1,21 @@
-import DiscordIcon from '@/assets/icon/socials/akar-icons_discord-fill.svg'
-import RedditIcon from '@/assets/icon/socials/akar-icons_reddit-fill.svg'
-import TelegramIcon from '@/assets/icon/socials/bxl_telegram.svg'
-import LinktreeIcon from '@/assets/icon/socials/simple-icons_linktree.svg'
-import TwitterIcon from '@/assets/icon/socials/akar-icons_twitter-fill.svg'
-import MediumIcon from '@/assets/icon/socials/bi_medium.svg'
-import LeftIcon from '@/assets/icon/go-back-icon.svg'
-import QuestionIcon from '@/assets/icon/icon--question.svg'
-import InfoIcon from '@/assets/icon/icon--info.svg'
+import DiscordIcon from "@/assets/icon/discord.svg";
+import GoBackIcon from "@/assets/icon/goBack.svg";
+import InfoIcon from "@/assets/icon/info.svg";
+import LinktreeIcon from "@/assets/icon/linktree.svg";
+import MediumIcon from "@/assets/icon/medium.svg";
+import QuestionIcon from "@/assets/icon/question.svg";
+import RedditIcon from "@/assets/icon/reddit.svg";
+import TelegramIcon from "@/assets/icon/telegram.svg";
+import TwitterIcon from "@/assets/icon/twitter.svg";
 
 export {
-	DiscordIcon,
-	RedditIcon,
-	TelegramIcon,
-	LinktreeIcon,
-	TwitterIcon,
-	MediumIcon,
-	LeftIcon,
-	QuestionIcon,
-	InfoIcon,
-}
+    DiscordIcon,
+    GoBackIcon,
+    InfoIcon,
+    LinktreeIcon,
+    MediumIcon,
+    QuestionIcon,
+    RedditIcon,
+    TelegramIcon,
+    TwitterIcon,
+};
