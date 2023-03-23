f565d5da0a492731f9637c7923ada59e7a591a0e
dmi <dmi.villione@gmail.com>
2023-03-23 10:45:44 +0300

    add Image component

---
+++ src/components/Image/Image.tsx
@@ -0,0 +1,11 @@
+import { Image as Img } from '@chakra-ui/react'
+import React from 'react'
+import styles from './Image.module.scss'
+
+export type ImgSize = 'auto' | 's' | 'm' | 'l'
+
+export const Image: React.FCC<
+	React.ComponentProps<typeof Img> & { size: ImgSize }
+> = ({ size = 'auto', ...props }) => {
+	return <Img className={styles[size]} {...props} />
+}
