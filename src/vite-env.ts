39e6e3a5c0c840ce9967eec531c7e2fb0816592a
dmi <dmi.villione@gmail.com>
2023-03-24 19:54:54 +0300

    add env declaration

---
+++ src/vite-env.ts
@@ -0,0 +1,8 @@
+declare const VITE_ALCHEMY_API_KEY: string
+export const ALCHEMY_API_KEY = VITE_ALCHEMY_API_KEY
+
+declare const VITE_APP_VERSION: string
+export const APP_VERSION = VITE_APP_VERSION
+
+declare const VITE_GIT_COMMIT: string
+export const GIT_COMMIT = VITE_GIT_COMMIT
