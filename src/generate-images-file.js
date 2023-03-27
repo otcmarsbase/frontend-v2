bc62fe3b73fb2c31fc30ebc5d391e055af41df52
dmi <dmi.villione@gmail.com>
2023-03-27 17:57:14 +0300

    update comments

--- src/generate-images-file.js
+++ src/generate-images-file.js
@@ -1,9 +1,19 @@
 /**
  * @example
- * node generate-images-file.js ./assets/icon/socials ./images.tsx %file%Icon flag
- * will generate images.tsx that contains import %file%Icon from "assets/icon/socials",
- * %file% is replacement for original filename
- * flag -- file system flag, default "w" https://nodejs.org/api/fs.html#file-system-flags
+ * Принимает аргументы:
+ * 1) путь от src до папки с файлами = folderPath
+ * 2) локация для сгенерированного файла
+ * 3) маска названия файла. В строке вхождение %(any string)% заменяется на названи файла. 
+ *    Например маска %file%Icon к любому имени файла добавит суффикс Icon
+ * 4) необязательный флаг открытия сгенерированного файла(чтобы дополнять/переписывать файл). По умолчанию равен w
+ * https://nodejs.org/api/fs.html#file-system-flags
+ * Скрипт генерирует файл с импортами картинок в формате 
+ * import FileName from "@/{folderPath}"
+ * export {
+ *  FileName,
+ * }
+ * 
+ * node generate-images-file.js ./assets/icon/ ./images.tsx %file%Icon w
  */
 
 const fs = require('fs')
@@ -12,13 +22,10 @@ const prettier = require('prettier')
 
 const pretify = (str) => prettier.format(str, { parser: 'babel', tabWidth: 4 })
 
-// Specify the folder path and output file path
 const folderPath = path.join(__dirname, process.argv[2])
 const outputPath = path.join(__dirname, process.argv[3])
 
-/*
- %importName%Icon => NameIcon
-*/
+
 const importMask = process.argv[4]
 const openFileFlag = process.argv[5] || 'w'
 const srcPath = path.join(__dirname, './')
@@ -26,7 +33,6 @@ const importPrefix = `@${folderPath.replace(srcPath, '/')}`
 
 const files = fs.readdirSync(folderPath)
 
-// Filter out non-image files
 const imageFiles = files.filter((file) =>
 	['.png', '.jpg', '.jpeg', '.gif', '.svg'].includes(path.extname(file))
 )
@@ -40,19 +46,16 @@ const mask = (file) =>
 			capitalizeFirstLetter(filename(file))
 		)
 	)
-// Generate the import statements
 const importStatements = imageFiles
 	.map(
 		(file) => `import ${mask(file)} from "${path.join(importPrefix, file)}"`
 	)
 	.join('\n')
 
-// Generate the exports statement
 const exportsStatement = `export {\n${imageFiles
 	.map((file) => `${mask(file)},`)
 	.join('\n')}\n}`
 
-// Write the output file
 fs.writeFileSync(
 	outputPath,
 	pretify(`${importStatements}\n\n${exportsStatement}`, {
