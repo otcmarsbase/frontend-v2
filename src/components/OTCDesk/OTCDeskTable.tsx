e534c581b96cd434301b8fb7451578b58acd9204
dmi <dmi.villione@gmail.com>
2023-03-27 19:15:09 +0300

    big value

--- src/components/OTCDesk/OTCDeskTable.tsx
+++ src/components/OTCDesk/OTCDeskTable.tsx
@@ -67,7 +67,7 @@ export const OTCDeskTable: React.FC<OTCDeskTableProps> = ({}) => {
 				<TableBodyItem
 					data={[
 						{ title: 'From', value: 1 },
-						{ title: 'To', value: 2 },
+						{ title: 'To', value: 21232132132132121321 },
 						{ title: 'Offer mode', value: 3 },
 						{ title: 'Available size', value: 4 },
 						{ title: 'Discount', value: 5 },
