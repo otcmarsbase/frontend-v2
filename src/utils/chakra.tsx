2635fcd4b537a94e56353ff9b2fa1ba3ebb8d7cb
dmi <dmi.villione@gmail.com>
2023-03-28 16:25:10 +0300

    extend chakra breakpoints

--- src/utils/chakra.tsx
+++ src/utils/chakra.tsx
@@ -1,20 +1,26 @@
-import { extendTheme, type ThemeConfig } from "@chakra-ui/react"
+import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
 
 const config: ThemeConfig = {
-	initialColorMode: "dark",
-	useSystemColorMode: false
+	initialColorMode: 'dark',
+	useSystemColorMode: false,
 }
 
+const breakpoints = {
+	sm: '320px', // 480px
+	md: '712px', // 768px
+	lg: '1200px', // 992px
+}
 export const theme = extendTheme({
+	breakpoints,
 	config,
 	styles: {
 		global: () => ({
 			body: {
-				bg: "var(--color-dark-950)",
-				fontFamily: "var(--font-main)",
-				color: "var(--color-white)",
-				lineHeight: "var(--line-height-xl)"
-			}
-		})
-	}
+				bg: 'var(--color-dark-950)',
+				fontFamily: 'var(--font-main)',
+				color: 'var(--color-white)',
+				lineHeight: 'var(--line-height-xl)',
+			},
+		}),
+	},
 })
