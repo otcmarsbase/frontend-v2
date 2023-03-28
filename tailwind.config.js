bb6d6ac8dfadc615ce9061b6ea4342041f5e631a
dmi <dmi.villione@gmail.com>
2023-03-28 16:22:35 +0300

    upd tailwind breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,29 +6,9 @@ module.exports = {
         colors:{
         },
         screens: {
-          '320-v': { "raw": "(orientation: portrait) and (min-width: 320px)" },
-          '360-v': { "raw": "(orientation: portrait) and (min-width: 360px)" },
-          '375-v': { "raw": "(orientation: portrait) and (min-width: 375px)" },
-          '390-v': { "raw": "(orientation: portrait) and (min-width: 390px)" },
-          '412-v':  { "raw": "(orientation: portrait) and (min-width: 412px)" },
-          "tablet-v": { "raw": "(orientation: portrait) and (min-width: 712px)" },
-          "lg-tablet-v": { "raw": "(orientation: portrait) and (min-width: 1024px)" },
-  
-          "320-h": { "raw": "(orientation: landscape) and (min-height: 320px)" },
-          "360-h": { "raw": "(orientation: landscape) and (min-height: 360px)" },
-          "375-h": { "raw": "(orientation: landscape) and (min-height: 375px)" },
-          "390-h": { "raw": "(orientation: landscape) and (min-height: 390px)" },
-          "412-h": { "raw": "(orientation: landscape) and (min-height: 412px)" },
-          "tablet-h": { "raw": "(orientation: landscape) and (min-height: 712px)" },
-          "lg-tablet-h": { "raw": "(orientation: landscape) and (min-height: 1024px)" },
-  
-          "320-vh": { "raw": "(orientation: landscape) and (min-height: 320px), (orientation: portrait) and (min-width: 320px)" },
-          "360-vh": { "raw": "(orientation: landscape) and (min-height: 360px), (orientation: portrait) and (min-width: 360px)" },
-          "375-vh": { "raw": "(orientation: landscape) and (min-height: 375px), (orientation: portrait) and (min-width: 375px)" },
-          "390-vh": { "raw": "(orientation: landscape) and (min-height: 390px), (orientation: portrait) and (min-width: 390px)" },
-          "412-vh": { "raw": "(orientation: landscape) and (min-height: 412px), (orientation: portrait) and (min-width: 412px)" },
-          "tablet-vh": { "raw": "(orientation: landscape) and (min-height: 712px), (orientation: portrait) and (min-width: 712px)" },
-          "lg-tablet-vh": { "raw": "(orientation: landscape) and (min-height: 1024px), (orientation: portrait) and (min-width: 1024px)" },
+          'sm': { "raw": "(orientation: portrait) and (min-width: 320px)" },
+          'md': { "raw": "(orientation: portrait) and (min-width: 712px)" },
+          'lg': { "raw": "(orientation: portrait) and (min-width: 1200px)" },
         },
         boxShadow: {
          
