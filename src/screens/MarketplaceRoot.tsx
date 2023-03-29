59ab2f817911a33c57bd92d0b6c13cc4082bb7c5
dmi <dmi.villione@gmail.com>
2023-03-29 13:39:10 +0300

    upd marketplace screen

--- src/screens/MarketplaceRoot.tsx
+++ src/screens/MarketplaceRoot.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import { CreateOfferHeader } from "@/components/CreateOfferHeader/CreateOfferHeader"
+import { MPContent } from "@/components/Marketplace/MPContent"
+import { ScreenWrapper } from "@/components/ScreenWrapper/ScreenWrapper"
+import { BaseText } from "@/components/Text/BaseText"
+import { useTranslation } from "@/localization/l10n"
+import React from "react"
 
 type MarketplaceRootProps = {}
 
 export const MarketplaceRoot: React.FC<MarketplaceRootProps> = ({}) => {
-    return <div>MarketplaceRoot</div>
+	const l10n = useTranslation()
+	return (
+		<ScreenWrapper
+			top={
+				<CreateOfferHeader
+					createOfferBtn={{
+						label: l10n.OTCDesk.createOfferLabel,
+						onClick: () => {},
+					}}
+					subTitle={<BaseText>{l10n.OTCDesk.subTitle}</BaseText>}
+					title={l10n.OTCDesk.title}
+				/>
+			}
+		>
+			<MPContent />
+		</ScreenWrapper>
+	)
 }
