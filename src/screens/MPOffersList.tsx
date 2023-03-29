d3e78be53a965c6360938d730eb6b1dc7c59b995
dmi <dmi.villione@gmail.com>
2023-03-29 13:55:17 +0300

    add mp offers list

--- src/screens/MPOffersList.tsx
+++ src/screens/MPOffersList.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import { CreateOfferHeader } from "@/components/CreateOfferHeader/CreateOfferHeader"
+import { MPOffersTable } from "@/components/Marketplace/MPOffersTable"
+import { ScreenWrapper } from "@/components/ScreenWrapper/ScreenWrapper"
+import { BaseText } from "@/components/Text/BaseText"
+import { useTranslation } from "@/localization/l10n"
+import React from "react"
 
 type MPOffersListProps = {}
 
 export const MPOffersList: React.FC<MPOffersListProps> = ({}) => {
-    return <div>MPOffersList</div>
+	const l10n = useTranslation()
+	return (
+		<ScreenWrapper
+			top={
+				<CreateOfferHeader
+					createOfferBtn={{
+						label: l10n.marketplace.header.offerBtn,
+						onClick: () => {},
+					}}
+					subTitle={
+						<BaseText>{l10n.marketplace.header.subitle}</BaseText>
+					}
+					title={l10n.marketplace.header.title}
+				/>
+			}
+		>
+			<MPOffersTable />
+		</ScreenWrapper>
+	)
 }
