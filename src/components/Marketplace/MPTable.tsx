806c601dec50e5a1b48f8cbe72404ac8c3ad225e
dmi <dmi.villione@gmail.com>
2023-03-29 13:48:22 +0300

    add Marketplace table row

--- src/components/Marketplace/MPTable.tsx
+++ src/components/Marketplace/MPTable.tsx
@@ -1,8 +1,65 @@
-import { Table } from "@/components/Table/Table"
+import { Table, TableRow } from "@/components/Table/Table"
+import { Box, HStack, VStack } from "@chakra-ui/react"
 import React from "react"
 
 type MPTableProps = {}
 
 export const MPTable: React.FC<MPTableProps> = ({}) => {
 	return <Table body={[]} header={[]} />
+}
+
+type MPRow = {
+	offerId: {
+		title: string
+		value: any
+	}
+	amount: {
+		title: string
+		value: any
+	}
+	acceptedTokens: {
+		title: string
+		value: any
+	}
+	bids: {
+		title: string
+		value: any
+	}
+	viewOffer: React.ReactNode
+}
+const MPRow: React.FC<MPRow> = ({
+	acceptedTokens,
+	amount,
+	bids,
+	offerId,
+	viewOffer,
+}) => {
+	return (
+		<TableRow
+			rowData={[
+				offerId.value,
+				amount.value,
+				acceptedTokens.value,
+				bids.value,
+				viewOffer,
+			]}
+			cardData={
+				<VStack border="1px solid red">
+					{[offerId, amount, acceptedTokens, bids].map((x) => (
+						<HStack w={"100%"} justifyContent={"space-between"}>
+							<Box>{x.title}</Box>
+							<Box>{x.value}</Box>
+						</HStack>
+					))}
+					<Box
+						w={"100%"}
+						paddingTop="10px"
+						borderTop="1px solid #2A2A2C"
+					>
+						{viewOffer}
+					</Box>
+				</VStack>
+			}
+		/>
+	)
 }
