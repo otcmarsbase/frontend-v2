ad950db01cb9e8b4a7d1b1f3256fcf52e210e89d
dmi <dmi.villione@gmail.com>
2023-03-29 13:29:03 +0300

    add mp table

---
+++ src/components/Marketplace/MPTable.tsx
@@ -0,0 +1,8 @@
+import { Table } from "@/components/Table/Table"
+import React from "react"
+
+type MPTableProps = {}
+
+export const MPTable: React.FC<MPTableProps> = ({}) => {
+	return <Table body={[]} header={[]} />
+}
