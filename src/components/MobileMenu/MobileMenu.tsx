3dd5f16ea8a7554faab9c7e3ef1416e97d482269
dmi <dmi.villione@gmail.com>
2023-03-31 19:46:40 +0300

    add mobileMenu

---
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react"
+import { Spin as Hamburger } from "hamburger-react"
+import React from "react"
+
+type MobileMenuProps = {}
+
+export const MobileMenu: React.FC<MobileMenuProps> = ({}) => {
+	return <Box></Box>
+}
