8fbdb2e629d72af7a157cf57cc4c0552fe484e3f
dmi <dmi.villione@gmail.com>
2023-04-03 11:30:08 +0300

    [MobileFooter] wrap MobileView

--- src/components/MobileFooter/MobileFooter.tsx
+++ src/components/MobileFooter/MobileFooter.tsx
@@ -1,3 +1,6 @@
+import { MobileView } from "@/components/MobileView"
+import { HStack } from "@chakra-ui/react"
+import React from "react"
 import { useLocation, useNavigate } from "react-router-dom"
 
 type MobileFooterProps = {}
@@ -6,4 +9,8 @@ export const MobileFooter: React.FC<MobileFooterProps> = ({}) => {
     const navigate = useNavigate()
     const location = useLocation()
 	return (
+		<MobileView>
+			<HStack></HStack>
+		</MobileView>
+	)
 }
