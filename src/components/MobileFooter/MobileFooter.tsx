1cff55c8750826da196753e3b5afdd7593a529c8
dmi <dmi.villione@gmail.com>
2023-04-03 11:31:42 +0300

    [MobileFooter] add button

--- src/components/MobileFooter/MobileFooter.tsx
+++ src/components/MobileFooter/MobileFooter.tsx
@@ -1,16 +1,22 @@
+import { Image } from "@/components/Image/Image"
 import { MobileView } from "@/components/MobileView"
-import { HStack } from "@chakra-ui/react"
+import { Box, HStack } from "@chakra-ui/react"
 import React from "react"
 import { useLocation, useNavigate } from "react-router-dom"
 
 type MobileFooterProps = {}
 
 export const MobileFooter: React.FC<MobileFooterProps> = ({}) => {
-    const navigate = useNavigate()
-    const location = useLocation()
+	const navigate = useNavigate()
+	const location = useLocation()
 	return (
 		<MobileView>
 			<HStack></HStack>
 		</MobileView>
 	)
+}
+
+type MobileFooterButtonProps = {}
+const MobileFooterButton: React.FC<MobileFooterButtonProps> = ({}) => {
+	return <Box></Box>
 }
