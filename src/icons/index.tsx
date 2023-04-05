a59b7305441e91c21eaa2c8d23cd74d12c45cfbd
dmi <dmi.villione@gmail.com>
2023-04-05 16:12:11 +0300

    generate icons

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,21 +1,27 @@
+import ArrowsIcon from "@/assets/icon/arrows.svg";
 import DiscordIcon from "@/assets/icon/discord.svg";
+import FilterIcon from "@/assets/icon/filter.svg";
 import InfoIcon from "@/assets/icon/info.svg";
 import LeftArrowIcon from "@/assets/icon/leftArrow.svg";
 import LinktreeIcon from "@/assets/icon/linktree.svg";
 import MediumIcon from "@/assets/icon/medium.svg";
 import QuestionIcon from "@/assets/icon/question.svg";
 import RedditIcon from "@/assets/icon/reddit.svg";
+import SearchIcon from "@/assets/icon/search.svg";
 import TelegramIcon from "@/assets/icon/telegram.svg";
 import TwitterIcon from "@/assets/icon/twitter.svg";
 
 export {
+    ArrowsIcon,
     DiscordIcon,
+    FilterIcon,
     InfoIcon,
     LeftArrowIcon,
     LinktreeIcon,
     MediumIcon,
     QuestionIcon,
     RedditIcon,
+    SearchIcon,
     TelegramIcon,
     TwitterIcon,
 };
