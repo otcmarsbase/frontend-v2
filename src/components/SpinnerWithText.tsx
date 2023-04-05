4febcc07073ebb13f516a53457998fe764b3ca14
dmi <dmi.villione@gmail.com>
2023-04-05 16:38:39 +0300

    change baseText component

--- src/components/SpinnerWithText.tsx
+++ src/components/SpinnerWithText.tsx
@@ -1,4 +1,5 @@
-import { Flex, Spinner, Text, VStack } from "@chakra-ui/react"
+import { BaseText } from "@/components/Text/BaseText"
+import { Flex, Spinner, VStack } from "@chakra-ui/react"
 
 export const SpinnerWithText: React.FCC<{ height?: string }> = ({
 	height,
@@ -7,7 +8,7 @@ export const SpinnerWithText: React.FCC<{ height?: string }> = ({
 	<Flex justify="center" align="center" minH={height || "100vh"}>
 		<VStack>
 			<Spinner size="xl" />
-			<Text>{children}</Text>
+			<BaseText>{children}</BaseText>
 		</VStack>
 	</Flex>
 )
