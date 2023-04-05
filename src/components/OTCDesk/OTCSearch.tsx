fbfab738d0c5b3c3147948519d5f1a40276cd992
dmi <dmi.villione@gmail.com>
2023-04-05 16:13:15 +0300

    add OTCSearch components

--- src/components/OTCDesk/OTCSearch.tsx
+++ src/components/OTCDesk/OTCSearch.tsx
@@ -1,3 +1,63 @@
+import {
+	SelectOptionType,
+	SelectV2,
+	SelectV2Props,
+} from "@/components/Select/Select"
+import { SearchIcon } from "@/icons"
+import {
+	components,
+	MenuProps,
+	OptionProps,
+	SingleValueProps,
+	ValueContainerProps,
+} from "react-select"
+
+interface OTCSearchOptionType extends SelectOptionType {
+	currencyImg: string
+	shortName: React.ReactNode
+	fullName: React.ReactNode
+	isClerable?: boolean
+}
+
+const OTCSearchOption = (props: OptionProps<OTCSearchOptionType>) => (
+	<div>
+		<components.Option {...props}>
+			<img src={props.data.currencyImg} alt="" />
+			<span>{props.data.shortName}</span>
+			<span>{props.data.fullName}</span>
+		</components.Option>
+	</div>
+)
+
+const OTCSearchSingleValue = ({
+	children,
+	...props
+}: SingleValueProps<OTCSearchOptionType>) => (
+	<components.SingleValue {...props}>
+		{" "}
+		<img src={props.data.currencyImg} alt="currency img" /> {children}
+	</components.SingleValue>
+)
+
+const OTCSearchMenu = (props: MenuProps<OTCSearchOptionType>) => (
+	<components.Menu {...props} />
+)
+
+const OTCSearchValueContainer = (
+	props: ValueContainerProps<OTCSearchOptionType>
+) => (
+	<>
+		<img src={SearchIcon} alt="search img" />
+		<div style={{ flexGrow: "1" }}>
+			<components.ValueContainer {...props} />
+		</div>
+	</>
+)
+
+export type OTCSearchProps = Omit<
+	SelectV2Props<OTCSearchOptionType>,
+	"isSearcheable"
+>
 
 export const OTCSearch: React.FC<React.PropsWithChildren<OTCSearchProps>> = (
 	props
