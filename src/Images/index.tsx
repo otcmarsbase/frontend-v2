7992472579e7f7e5fa2f7582a55ea944c0b68583
dmi <dmi.villione@gmail.com>
2023-04-05 15:35:29 +0300

    upd generator img

--- src/Images/index.tsx
+++ src/Images/index.tsx
@@ -1,3 +1,6 @@
-import MetamaskImg from "@/assets/images/metamask.png";
+import LoaderErrorImg from "@/assets/images/loaderError.gif"
+import LoaderLoadingImg from "@/assets/images/loaderLoading.gif"
+import LoaderOkImg from "@/assets/images/loaderOk.gif"
+import MetamaskImg from "@/assets/images/metamask.png"
 
-export { MetamaskImg };
+export { LoaderErrorImg, LoaderLoadingImg, LoaderOkImg, MetamaskImg }
