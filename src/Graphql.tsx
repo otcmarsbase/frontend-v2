1a762acd14e3887eb59812bbed81a44051d7a1e2
dmi <dmi.villione@gmail.com>
2023-04-06 18:32:38 +0300

    add grapql query client

---
+++ src/Graphql.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { createClient } from "urql"
+import { QueryClient } from "@tanstack/react-query"
+
+export const client = createClient({
+    url: "https://hasura.otcmarsbase.io/v1/graphql",
+    exchanges: []
+})
+
+export const queryClient = new QueryClient()
