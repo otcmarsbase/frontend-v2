ed8f55edd0d048e05153b56a738d0fee95448848
dmi <dmi.villione@gmail.com>
2023-04-06 18:32:38 +0300

    add query cloent provider

--- src/Graphql.tsx
+++ src/Graphql.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { createClient } from "urql"
 import { QueryClient } from "@tanstack/react-query"
+import { QueryClientProvider } from "@tanstack/react-query"
 
 export const client = createClient({
     url: "https://hasura.otcmarsbase.io/v1/graphql",
