9bb414dd561fbb2023e84afa2ca5b95031202fe2
dmi <dmi.villione@gmail.com>
2023-04-06 18:32:38 +0300

    update file generator

--- src/generate-images-file.js
+++ src/generate-images-file.js
@@ -3,43 +3,52 @@
  * Принимает аргументы:
  * 1) путь от src до папки с файлами = folderPath
  * 2) локация для сгенерированного файла
- * 3) маска названия файла. В строке вхождение %(any string)% заменяется на названи файла. 
+ * 3) маска названия файла. В строке вхождение %(any string)% заменяется на названи файла.
  *    Например маска %file%Icon к любому имени файла добавит суффикс Icon
  * 4) необязательный флаг открытия сгенерированного файла(чтобы дополнять/переписывать файл). По умолчанию равен w
- * https://nodejs.org/api/fs.html#file-system-flags
- * Скрипт генерирует файл с импортами картинок в формате 
+ *     (https://nodejs.org/api/fs.html#file-system-flags)
+ * 5) формат импортов, svg или img, для svg генерируется особый импорт вида
+ *           import from { ReactComponent as %filename% }. Это надо для работы плагина svgr
+ *
+ *
+ * Скрипт генерирует файл с импортами картинок в формате
  * import FileName from "@/{folderPath}"
  * export {
  *  FileName,
  * }
- * 
+ *
  * node generate-images-file.js ./assets/icon/ ./images.tsx %file%Icon w
  */
 
-const fs = require('fs')
-const path = require('path')
-const prettier = require('prettier')
+const fs = require("fs")
+const path = require("path")
+const prettier = require("prettier")
 
-const pretify = (str) => prettier.format(str, { parser: 'babel', tabWidth: 4 })
+const pretify = (str) => prettier.format(str, { parser: "babel", tabWidth: 4 })
 
 const folderPath = path.join(__dirname, process.argv[2])
 const outputPath = path.join(__dirname, process.argv[3])
 
-
 const importMask = process.argv[4]
-const openFileFlag = process.argv[5] || 'w'
-const srcPath = path.join(__dirname, './')
-const importPrefix = `@${folderPath.replace(srcPath, '/')}`
+const openFileFlag = process.argv[5] || "w"
+const format = process.argv[6] || "img"
+const srcPath = path.join(__dirname, "./")
+const importPrefix = `@${folderPath.replace(srcPath, "/")}`
 
 const files = fs.readdirSync(folderPath)
 
 const imageFiles = files.filter((file) =>
-	['.png', '.jpg', '.jpeg', '.gif', '.svg'].includes(path.extname(file))
+	[".png", ".jpg", ".jpeg", ".gif", ".svg"].includes(path.extname(file))
 )
 const capitalizeFirstLetter = (string) =>
 	string.charAt(0).toUpperCase() + string.slice(1)
 const filename = (file) => path.parse(file).name
 
+const importPaths = {
+	svg: (filename, path) =>
+		`import { ReactComponent as ${filename} } from "${path}"`,
+	img: (filename, path) => `import ${filename} from "${path}"`,
+}
 const mask = (file) =>
 	capitalizeFirstLetter(
 		importMask.replace(/%(.*)%/, () =>
@@ -47,14 +56,14 @@ const mask = (file) =>
 		)
 	)
 const importStatements = imageFiles
-	.map(
-		(file) => `import ${mask(file)} from "${path.join(importPrefix, file)}"`
+	.map((file) =>
+		importPaths[format](mask(file), path.join(importPrefix, file))
 	)
-	.join('\n')
+	.join("\n")
 
 const exportsStatement = `export {\n${imageFiles
 	.map((file) => `${mask(file)},`)
-	.join('\n')}\n}`
+	.join("\n")}\n}`
 
 fs.writeFileSync(
 	outputPath,
