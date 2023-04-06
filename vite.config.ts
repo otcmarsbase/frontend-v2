b38daa58f8daaf8d7061b23882d0cd850edbe39b
dmi <dmi.villione@gmail.com>
2023-04-06 18:32:38 +0300

    add vite-plugin-svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import react from '@vitejs/plugin-react'
-const packageJson = require('./package.json')
-import { defineConfig } from 'vite'
-import checker from 'vite-plugin-checker'
-import git from 'git-rev-sync'
-import rollupNodePolyfills from 'rollup-plugin-node-polyfills'
-import path from 'path'
+import react from "@vitejs/plugin-react"
+const packageJson = require("./package.json")
+import { defineConfig } from "vite"
+import checker from "vite-plugin-checker"
+import git from "git-rev-sync"
+import rollupNodePolyfills from "rollup-plugin-node-polyfills"
+import path from "path"
+import svgr from "vite-plugin-svgr"
 
 let commit = git.short()
 
@@ -12,24 +13,26 @@ const version = packageJson.version
 console.log(`Building app v${version} (${commit})`)
 
 export default defineConfig({
-	base: '',
+	base: "",
 	define: {
 		VITE_APP_VERSION: JSON.stringify(version),
 		VITE_GIT_COMMIT: JSON.stringify(commit),
 		VITE_ALCHEMY_API_KEY: JSON.stringify("key"),
 	},
 	build: {
-		assetsDir: '',
+		assetsDir: "",
 	},
 	plugins: [
 		react({ fastRefresh: false }),
+		svgr(),
+
 		checker({
 			typescript: true,
 		}),
 	],
 	resolve: {
 		alias: [
-			...[/* "events", */ /* "path", */ /* "util", */ 'url', 'http'].map(
+			...[/* "events", */ /* "path", */ /* "util", */ "url", "http"].map(
 				(x) => ({
 					find: x,
 					replacement: `rollup-plugin-node-polyfills/polyfills/${x}`,
@@ -37,18 +40,18 @@ export default defineConfig({
 			),
 			// { find: 'child_process', replacement: 'rollup-plugin-node-polyfills' },
 			{
-				find: 'https',
-				replacement: 'rollup-plugin-node-polyfills/polyfills/http',
+				find: "https",
+				replacement: "rollup-plugin-node-polyfills/polyfills/http",
 			},
 			{
-				find: 'querystring',
-				replacement: 'rollup-plugin-node-polyfills/polyfills/qs',
+				find: "querystring",
+				replacement: "rollup-plugin-node-polyfills/polyfills/qs",
 			},
-			{ find: 'process', replacement: 'process/browser' },
-			{ find: 'stream', replacement: 'stream-browserify' },
-			{ find: 'zlib', replacement: 'browserify-zlib' },
-			{ find: 'util', replacement: 'util/util' },
-			{ find: '@', replacement: path.resolve(__dirname, 'src') },
+			{ find: "process", replacement: "process/browser" },
+			{ find: "stream", replacement: "stream-browserify" },
+			{ find: "zlib", replacement: "browserify-zlib" },
+			{ find: "util", replacement: "util/util" },
+			{ find: "@", replacement: path.resolve(__dirname, "src") },
 		],
 	},
 })
