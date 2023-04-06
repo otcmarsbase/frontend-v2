7175bead62a7b43f2fc331133e0e3cb753f3bae0
dmi <dmi.villione@gmail.com>
2023-04-06 18:32:38 +0300

    renegerate icons

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,17 +1,17 @@
-import ArrowsIcon from "@/assets/icon/arrows.svg";
-import DashboardIcon from "@/assets/icon/dashboard.svg";
-import DiscordIcon from "@/assets/icon/discord.svg";
-import FilterIcon from "@/assets/icon/filter.svg";
-import InfoIcon from "@/assets/icon/info.svg";
-import LeftArrowIcon from "@/assets/icon/leftArrow.svg";
-import LinktreeIcon from "@/assets/icon/linktree.svg";
-import MediumIcon from "@/assets/icon/medium.svg";
-import QuestionIcon from "@/assets/icon/question.svg";
-import RedditIcon from "@/assets/icon/reddit.svg";
-import SearchIcon from "@/assets/icon/search.svg";
-import SwapIcon from "@/assets/icon/swap.svg";
-import TelegramIcon from "@/assets/icon/telegram.svg";
-import TwitterIcon from "@/assets/icon/twitter.svg";
+import { ReactComponent as ArrowsIcon } from "@/assets/icon/arrows.svg"
+import { ReactComponent as DashboardIcon } from "@/assets/icon/dashboard.svg"
+import { ReactComponent as DiscordIcon } from "@/assets/icon/discord.svg"
+import { ReactComponent as FilterIcon } from "@/assets/icon/filter.svg"
+import { ReactComponent as InfoIcon } from "@/assets/icon/info.svg"
+import { ReactComponent as LeftArrowIcon } from "@/assets/icon/leftArrow.svg"
+import { ReactComponent as LinktreeIcon } from "@/assets/icon/linktree.svg"
+import { ReactComponent as MediumIcon } from "@/assets/icon/medium.svg"
+import { ReactComponent as QuestionIcon } from "@/assets/icon/question.svg"
+import { ReactComponent as RedditIcon } from "@/assets/icon/reddit.svg"
+import { ReactComponent as SearchIcon } from "@/assets/icon/search.svg"
+import { ReactComponent as SwapIcon } from "@/assets/icon/swap.svg"
+import { ReactComponent as TelegramIcon } from "@/assets/icon/telegram.svg"
+import { ReactComponent as TwitterIcon } from "@/assets/icon/twitter.svg"
 
 export {
     ArrowsIcon,
@@ -28,4 +28,4 @@ export {
     SwapIcon,
     TelegramIcon,
     TwitterIcon,
-};
+}
