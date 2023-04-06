f8ee902b1c4040291c79cd50937a73c39951c4bb
dmi <dmi.villione@gmail.com>
2023-04-06 18:32:38 +0300

    add GraphqlContext

--- src/Graphql.tsx
+++ src/Graphql.tsx
@@ -1,11 +1,25 @@
 import React from "react"
 import { createClient } from "urql"
 import { QueryClient } from "@tanstack/react-query"
+import { Provider } from "urql"
+
 import { QueryClientProvider } from "@tanstack/react-query"
 
+const GraphqlContext: React.FC<
+	React.PropsWithChildren<React.PropsWithChildren>
+> = ({ children }) => {
+	return (
+		<Provider value={client}>
+			<QueryClientProvider client={queryClient}>
+				{children}
+			</QueryClientProvider>
+		</Provider>
+	)
+}
+
 export const client = createClient({
-    url: "https://hasura.otcmarsbase.io/v1/graphql",
-    exchanges: []
+	url: "https://hasura.otcmarsbase.io/v1/graphql",
+	exchanges: [],
 })
 
 export const queryClient = new QueryClient()
