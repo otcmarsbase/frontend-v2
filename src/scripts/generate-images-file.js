b31359086c491a02ec2a5c0c548fa17461364674
dmi <dmi.villione@gmail.com>
2023-04-07 11:05:20 +0300

    rollback generate-jmages-script

--- src/scripts/generate-images-file.js
+++ src/scripts/generate-images-file.js
@@ -22,9 +22,11 @@
 
 const fs = require("fs")
 const path = require("path")
-const utils = require("./utils.js")
+const prettier = require("prettier")
+const utls = require("./utils.js")
 
-
+const pretify = (str) =>
+	prettier.format(str, { parser: "babel", tabWidth: 4, semi: false })
 
 const dirName = path.join(__dirname, "..")
 const folderPath = path.join(dirName, process.argv[2])
@@ -32,7 +34,7 @@ const outputPath = path.join(dirName, process.argv[3])
 
 const importMask = process.argv[4]
 const openFileFlag = process.argv[5] || "w"
-
+const format = process.argv[6] || "img"
 const srcPath = path.join(dirName, "./")
 const importPrefix = `@${folderPath.replace(srcPath, "/")}`
 
@@ -44,22 +46,24 @@ const imageFiles = files.filter((file) =>
 
 
 const importPaths = {
+	svg: (filename, path) =>
+		`import { ReactComponent as ${filename} } from "${path}"`,
 	img: (filename, path) => `import ${filename} from "${path}"`,
 }
 
 const importStatements = imageFiles
 	.map((file) =>
-		importPaths["img"](utils.formatImportFile(file, importMask), path.join(importPrefix, file))
+		importPaths[format](utls.formatImportFile(file, importMask), path.join(importPrefix, file))
 	)
 	.join("\n")
 
 const exportsStatement = `export {\n${imageFiles
-	.map((file) => `${utils.formatImportFile(file, importMask)},`)
+	.map((file) => `${utls.formatImportFile(file, importMask)},`)
 	.join("\n")}\n}`
 
 fs.writeFileSync(
 	outputPath,
-	utils.pretify(`${importStatements}\n\n${exportsStatement}`, {
+	pretify(`${importStatements}\n\n${exportsStatement}`, {
 		flag: openFileFlag,
 	})
 )
