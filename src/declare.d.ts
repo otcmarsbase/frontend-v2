537954308dd29033d6ed40ea9af1c64f1324ad4b
dmi <dmi.villione@gmail.com>
2023-04-07 10:34:38 +0300

    hide svg declaration

--- src/declare.d.ts
+++ src/declare.d.ts
@@ -2,9 +2,10 @@ declare module "*.png"
 declare module "*.jpg"
 declare module "*.jpeg"
 declare module "*.gif"
+declare module "*.svg"
 declare module "*.scss"
 
-declare module "*.svg" {
-    import { ReactElement, SVGProps } from "react";
-    export const ReactComponent: (props: SVGProps<SVGElement>) => ReactElement;
-  }
+// declare module "*.svg" {
+//     import { ReactElement, SVGProps } from "react";
+//     export const ReactComponent: (props: SVGProps<SVGElement>) => ReactElement;
+//   }
