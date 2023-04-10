f9b5f1267a8bcaa1eb051d3761fe5afc26f631ae
dmi <dmi.villione@gmail.com>
2023-04-10 16:50:38 +0300

    add format util

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,1 +1,6 @@
-export const openExternalUrl = (url: string) => window.open(url, '_blank')
+export const openExternalUrl = (url: string) => window.open(url, "_blank")
+
+export const format = (template: string, ...params: string[]) =>
+	params.reduce((acc, cur, i) => {
+		return acc.replaceAll(`{${i}}`, cur)
+	}, template)
