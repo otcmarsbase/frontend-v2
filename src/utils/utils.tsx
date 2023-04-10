348bb1cb258cee08b23df5999ed779471eecddb9
dmi <dmi.villione@gmail.com>
2023-04-10 16:50:38 +0300

    add higlight component

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,6 +1,29 @@
+import { useHighlight } from "@chakra-ui/react"
+
 export const openExternalUrl = (url: string) => window.open(url, "_blank")
 
 export const format = (template: string, ...params: string[]) =>
 	params.reduce((acc, cur, i) => {
 		return acc.replaceAll(`{${i}}`, cur)
 	}, template)
+
+export const HighlightComponent: React.FC<{
+	template: string
+	components: ((key: string) => any)[]
+	query: string[]
+}> = ({ components, query, template }) => {
+	const chunks = useHighlight({
+		query: query,
+		text: template,
+	})
+	return (
+		<>
+			{chunks.map((chunk, i) => {
+				if (chunk.match) {
+					return components[i](chunk.text)
+				}
+				return chunk.text
+			})}
+		</>
+	)
+}
