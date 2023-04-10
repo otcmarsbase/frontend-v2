e23bb8b8892d83cbd1239fb14984bfcc30c13fa8
dmi <dmi.villione@gmail.com>
2023-04-10 16:50:38 +0300

    remove svg declaration

--- src/declare.d.ts
+++ src/declare.d.ts
@@ -5,7 +5,3 @@ declare module "*.gif"
 declare module "*.svg"
 declare module "*.scss"
 
-// declare module "*.svg" {
-//     import { ReactElement, SVGProps } from "react";
-//     export const ReactComponent: (props: SVGProps<SVGElement>) => ReactElement;
-//   }
