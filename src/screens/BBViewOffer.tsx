799ada4967502778f7f40ec4423d36b674337739
dmi <dmi.villione@gmail.com>
2023-04-10 16:50:38 +0300

    add bbviewoffer

--- src/screens/BBViewOffer.tsx
+++ src/screens/BBViewOffer.tsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import { flattenRoutes } from "@/AppRoutes"
+import { CreateOfferHeader } from "@/components/CreateOfferHeader/CreateOfferHeader"
+import { ScreenWrapper } from "@/components/ScreenWrapper/ScreenWrapper"
+import { BaseText } from "@/components/Text/BaseText"
+import { useTranslation } from "@/localization/l10n"
+import React from "react"
+import { Navigate, useParams } from "react-router-dom"
 
 type BBViewOfferProps = {
 	creatingBid: boolean
@@ -6,5 +12,26 @@ type BBViewOfferProps = {
 
 // TODO: Можно ли разделить экраны так, чтобы не прокидывать creatingBid?
 export const BBViewOffer: React.FC<BBViewOfferProps> = ({ creatingBid }) => {
-	return <div>BBViewOffer screen</div>
+	const l10n = useTranslation()
+
+	return (
+		<ScreenWrapper
+			top={
+				<CreateOfferHeader
+					backButton={{
+						label: "back to offer list",
+						onClick: () => {},
+					}}
+					createOfferBtn={{
+						label: l10n.marketplace.header.offerBtn,
+						onClick: () => {},
+					}}
+					subTitle={
+						<BaseText>{l10n.marketplace.header.subitle}</BaseText>
+					}
+					title={l10n.marketplace.header.title}
+				/>
+			}
+		></ScreenWrapper>
+	)
 }
