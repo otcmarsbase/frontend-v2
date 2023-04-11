1ecbd62abd26cedf9b646eb86f8aa338ba60ec81
dmi <dmi.villione@gmail.com>
2023-04-11 13:04:43 +0300

    add flex basis

--- src/components/CreateOffer/CreateOfferStep/CreateOfferStep.tsx
+++ src/components/CreateOffer/CreateOfferStep/CreateOfferStep.tsx
@@ -18,9 +18,8 @@ export const CreateOfferStep: React.FC<CreateOfferStepProps> = ({
 	return (
 		<Grid
 			autoFlow={"column"}
-			gap={4}
 			className={clsx(
-				"p-6 rounded-2xl basis-[340px] items-start justify-start ml-3",
+				"p-6 rounded-2xl flex-shrink-0 flex-grow-0 basis-[320px] items-start justify-start gap-4 ",
 				isActive && "bg-[rgba(37,38,40,.5)]"
 			)}
 		>
