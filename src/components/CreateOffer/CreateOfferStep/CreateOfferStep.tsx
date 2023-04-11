67cb27078f4f61af2ad128b9ea05e2ddf6317113
dmi <dmi.villione@gmail.com>
2023-04-11 12:27:46 +0300

    [CreateOfferStep] add active styles

--- src/components/CreateOffer/CreateOfferStep/CreateOfferStep.tsx
+++ src/components/CreateOffer/CreateOfferStep/CreateOfferStep.tsx
@@ -16,8 +16,8 @@ export const CreateOfferStep: React.FC<CreateOfferStepProps> = ({
 	isActive,
 }) => {
 	return (
-		<HStack>
-			<Box>
+		<HStack className={clsx("p-6 rounded-2xl", isActive && "bg-[rgba(37,38,40,.5)]")}>
+			<Box className={clsx("h-9 w-9 rounded-full flex justify-center items-center bg-dark300 text-black",isActive && "bg-mainGradient text-white")}>
 				<Text>{num}</Text>
 			</Box>
 			<Box>
