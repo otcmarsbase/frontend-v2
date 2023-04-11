0dd15344d2c578ac232a57e847f7fc7625920000
dmi <dmi.villione@gmail.com>
2023-04-11 11:04:33 +0300

    add create offer header

--- src/screens/CreateOfferNew.tsx
+++ src/screens/CreateOfferNew.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import { ScreenHeader } from "@/components/ScreenHeader/ScreenHeader"
+import { ScreenWrapper } from "@/components/ScreenWrapper/ScreenWrapper"
+import { BaseText } from "@/components/Text/BaseText"
+import { useTranslation } from "@/localization/l10n"
+import { HStack } from "@chakra-ui/react"
+import React from "react"
 
 type CreateOfferNewProps = {}
 
 export const CreateOfferNew: React.FC<CreateOfferNewProps> = ({}) => {
-    return <div>CreateOfferNew screen</div>
+	const l10n = useTranslation()
+	return (
+		<ScreenWrapper
+			top={
+				<>
+					<ScreenHeader
+						subTitle={<BaseText>{l10n.OTCDesk.subTitle}</BaseText>}
+						title={l10n.OTCDesk.title}
+					/>
+					<HStack>
+						<div>hello 1</div>
+                        <div>hello 2</div>
+                        <div>hello 3</div>
+
+					</HStack>
+				</>
+			}
+		></ScreenWrapper>
+	)
 }
