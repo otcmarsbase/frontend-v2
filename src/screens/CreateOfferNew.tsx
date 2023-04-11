2436d4b9eac30a2fc6f2af8654d24c88bad8fe5e
dmi <dmi.villione@gmail.com>
2023-04-11 13:04:34 +0300

    add steps scroll

--- src/screens/CreateOfferNew.tsx
+++ src/screens/CreateOfferNew.tsx
@@ -3,7 +3,7 @@ import { ScreenHeader } from "@/components/ScreenHeader/ScreenHeader"
 import { ScreenWrapper } from "@/components/ScreenWrapper/ScreenWrapper"
 import { BaseText } from "@/components/Text/BaseText"
 import { useTranslation } from "@/localization/l10n"
-import { Flex, HStack } from "@chakra-ui/react"
+import { Box, Flex, HStack } from "@chakra-ui/react"
 import React from "react"
 
 type CreateOfferNewProps = {}
@@ -18,24 +18,27 @@ export const CreateOfferNew: React.FC<CreateOfferNewProps> = ({}) => {
 						subTitle={<BaseText>{l10n.OTCDesk.subTitle}</BaseText>}
 						title={l10n.OTCDesk.title}
 					/>
-					<Flex alignItems={"stretch"} w={"100%"}>
-						<CreateOfferStep
-							isActive
-							num={1}
-							text="Set suitable conditions"
-							title="Parameter selection"
-						/>
-						<CreateOfferStep
-							num={2}
-							text="Verify that all parameters are set the way you want them"
-							title="Check offer details"
-						/>
-						<CreateOfferStep
-							num={3}
-							text="Send the offer to the market"
-							title="Publication"
-						/>
-					</Flex>
+
+					<Box overflow={"scroll"} w={"full"}>
+						<Flex alignItems={"stretch"}>
+							<CreateOfferStep
+								isActive
+								num={1}
+								text="Set suitable conditions"
+								title="Parameter selection"
+							/>
+							<CreateOfferStep
+								num={2}
+								text="Verify that all parameters are set the way you want them"
+								title="Check offer details"
+							/>
+							<CreateOfferStep
+								num={3}
+								text="Send the offer to the market"
+								title="Publication"
+							/>
+						</Flex>
+					</Box>
 				</>
 			}
 		></ScreenWrapper>
