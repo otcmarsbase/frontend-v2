d32be95bedce79e497e9838e6662cf339ec6863f
dmi <dmi.villione@gmail.com>
2023-04-13 11:43:44 +0300

    add input right component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,17 +1,40 @@
 import React from "react"
-import { Input as InputComponent } from "@chakra-ui/react"
-type InputProps = React.ComponentProps<typeof InputComponent>
+import {
+	Input as InputComponent,
+	InputGroup,
+	InputRightAddon,
+	InputRightElement,
+} from "@chakra-ui/react"
+import { useSize } from "@chakra-ui/react-use-size"
+type InputProps = React.ComponentProps<typeof InputComponent> & {
+	rightComponent?: React.ReactNode
+}
 
-export const Input: React.FC<InputProps> = (props) => {
+export const Input: React.FC<InputProps> = ({ rightComponent, ...props }) => {
+	const rightElementRef = React.useRef(null)
+	const rightSizes = useSize(rightElementRef)
 	return (
-		<InputComponent
-			bg={"transparent"}
-			borderWidth={"2px"}
-			style={{ outline: "none", boxShadow: "none" }}
-			className={
-				"border-dark-200 shadow-none focus-visible:!border-orange-100 hover:!border-orange-100 "
-			}
-			{...props}
-		/>
+		<InputGroup>
+			<InputComponent
+				pr={rightSizes?.width || "auto"}
+				bg={"transparent"}
+				borderWidth={"2px"}
+				style={{ outline: "none", boxShadow: "none" }}
+				className={
+					"border-dark-200 shadow-none focus-visible:!border-orange-100 hover:!border-orange-100 "
+				}
+				{...props}
+			/>
+			{rightComponent && (
+				<InputRightElement
+					paddingLeft={"4px"}
+					paddingRight={"16px"}
+					ref={rightElementRef}
+					w={"max-content"}
+				>
+					{rightComponent}
+				</InputRightElement>
+			)}
+		</InputGroup>
 	)
 }
