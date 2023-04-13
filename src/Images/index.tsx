740a44ef46494f54cfd965e9c3834a690b844736
dmi <dmi.villione@gmail.com>
2023-04-13 15:49:55 +0300

    regenerate images

--- src/Images/index.tsx
+++ src/Images/index.tsx
@@ -1,6 +1,30 @@
-import LoaderErrorImg from "@/assets/images/loaderError.gif"
-import LoaderLoadingImg from "@/assets/images/loaderLoading.gif"
-import LoaderOkImg from "@/assets/images/loaderOk.gif"
-import MetamaskImg from "@/assets/images/metamask.png"
+import LoaderErrorImgSource from "@/assets/images/loaderError.gif"
+import LoaderLoadingImgSource from "@/assets/images/loaderLoading.gif"
+import LoaderOkImgSource from "@/assets/images/loaderOk.gif"
+import MetamaskImgSource from "@/assets/images/metamask.png"
+import React from "react"
+import { Image } from "@/components/Image/Image"
 
-export { LoaderErrorImg, LoaderLoadingImg, LoaderOkImg, MetamaskImg }
+export const LoaderErrorImg: React.FC<React.ComponentProps<typeof Image>> = (
+    props
+) => {
+    return <Image src={LoaderErrorImgSource} {...props} />
+}
+
+export const LoaderLoadingImg: React.FC<React.ComponentProps<typeof Image>> = (
+    props
+) => {
+    return <Image src={LoaderLoadingImgSource} {...props} />
+}
+
+export const LoaderOkImg: React.FC<React.ComponentProps<typeof Image>> = (
+    props
+) => {
+    return <Image src={LoaderOkImgSource} {...props} />
+}
+
+export const MetamaskImg: React.FC<React.ComponentProps<typeof Image>> = (
+    props
+) => {
+    return <Image src={MetamaskImgSource} {...props} />
+}
