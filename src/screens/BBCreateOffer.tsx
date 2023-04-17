39e79caed8c94a5e950e7575ba840fc10cdc2941
dmi <dmi.villione@gmail.com>
2023-04-17 11:31:30 +0300

    add BBCreateOffer screen

--- src/screens/BBCreateOffer.tsx
+++ src/screens/BBCreateOffer.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import { BBCreateOfferContainer } from "@/components/BBCreateOffer/BBCreateOffer"
+import { ScreenHeader } from "@/components/ScreenHeader/ScreenHeader"
+import { ScreenWrapper } from "@/components/ScreenWrapper/ScreenWrapper"
+import { BaseText } from "@/components/Text/BaseText"
+import React from "react"
 
 type BBCreateOfferProps = {}
 
 export const BBCreateOffer: React.FC<BBCreateOfferProps> = ({}) => {
-    return <div>BBCreateOffer screen</div>
+	return (
+		<ScreenWrapper
+			top={
+				<ScreenHeader
+					subTitle={
+						<BaseText>
+							Create an auction available to everyone or share the
+							offer link with another investor to use Marsbase as
+							an escrow.
+						</BaseText>
+					}
+					title={"Create Best Bid Auction"}
+				/>
+			}
+		>
+			<div className="max-w-[790px] mx-auto">
+				<BBCreateOfferContainer
+					backButton={{
+						label: "Back to offer list",
+						onClick: () => {},
+					}}
+					tokenAlice={{
+						address: "0x000",
+						decimals: 1,
+						iconUrl: "https://via.placeholder.com/150",
+						name: "Etherium",
+						symbol: "ETH",
+					}}
+					tokensWillBeLocked={true}
+				/>
+			</div>
+		</ScreenWrapper>
+	)
 }
