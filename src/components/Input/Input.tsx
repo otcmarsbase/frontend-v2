730881886fa9dc8e73954f4ef60cda6dd40f6781
dmi <dmi.villione@gmail.com>
2023-04-17 11:31:29 +0300

    add input error msg

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,5 +1,7 @@
 import React from "react"
 import {
+	FormControl,
+	FormErrorMessage,
 	Input as InputComponent,
 	InputGroup,
 	InputRightAddon,
@@ -8,34 +10,43 @@ import {
 import { useSize } from "@chakra-ui/react-use-size"
 type InputProps = React.ComponentProps<typeof InputComponent> & {
 	rightComponent?: React.ReactNode
+	errorMsg?: string
 }
 
-export const Input: React.FC<InputProps> = ({ rightComponent, ...props }) => {
+export const Input: React.FC<InputProps> = ({
+	rightComponent,
+	isInvalid,
+	errorMsg,
+	...props
+}) => {
 	const rightElementRef = React.useRef(null)
 	const rightSizes = useSize(rightElementRef)
 	return (
-		<InputGroup>
-			<InputComponent
-				pr={rightSizes?.width || "auto"}
-				bg={"transparent"}
-				borderWidth={"2px"}
-				borderColor={"dark.200"}
-				style={{ outline: "none", boxShadow: "none" }}
-				className={
-					"!shadow-none focus-visible:!border-orange-100 hover:!border-orange-100 "
-				}
-				{...props}
-			/>
-			{rightComponent && (
-				<InputRightElement
-					paddingLeft={"4px"}
-					paddingRight={"16px"}
-					ref={rightElementRef}
-					w={"max-content"}
-				>
-					{rightComponent}
-				</InputRightElement>
-			)}
-		</InputGroup>
+		<FormControl isInvalid={isInvalid}>
+			<InputGroup>
+				<InputComponent
+					pr={rightSizes?.width || "auto"}
+					bg={"transparent"}
+					borderWidth={"2px"}
+					borderColor={"dark.200"}
+					style={{ outline: "none", boxShadow: "none" }}
+					className={
+						"!shadow-none focus-visible:!border-orange-100 hover:!border-orange-100 "
+					}
+					{...props}
+				/>
+				{rightComponent && (
+					<InputRightElement
+						paddingLeft={"4px"}
+						paddingRight={"16px"}
+						ref={rightElementRef}
+						w={"max-content"}
+					>
+						{rightComponent}
+					</InputRightElement>
+				)}
+			</InputGroup>
+			{isInvalid && <FormErrorMessage>{errorMsg}</FormErrorMessage>}
+		</FormControl>
 	)
 }
