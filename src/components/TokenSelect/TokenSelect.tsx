5a2b355c8e8b4c1f169e7b1365bd128e79e784af
dmi <dmi.villione@gmail.com>
2023-04-18 15:29:41 +0300

    add v

--- src/components/TokenSelect/TokenSelect.tsx
+++ src/components/TokenSelect/TokenSelect.tsx
@@ -1,10 +1,21 @@
-import { TokenInfo } from '@/types'
-import React from 'react'
-
+import { TokenInfo } from "@/types"
+import { either } from "@/utils/utils"
+import { CurrencyIcon } from "@otcmarsbase/react-components"
+import {
+	reactSelectBlackTheme,
+	ReactSelectIndexed,
+} from "../react-select-utils"
 
 export type TokenGroup = {
 	name: string
 	tokens: TokenInfo[]
+}
+export type TokenSelectorDropdownProps<T> = {
+	tokens: T[]
+	tokenSelectedIdx: number
+	onTokenSelected: (idx: number) => void
+	onTokenDeselected: () => void
+	disabled?: boolean
 }
 
 type TokenSelectProps = {}
@@ -12,3 +23,20 @@ type TokenSelectProps = {}
 export const TokenSelect: React.FC<TokenSelectProps> = ({}) => {
     return <div></div>
 }
+export const TokenSelectorDropdown = <T extends TokenInfo | TokenGroup>(
+	props: TokenSelectorDropdownProps<T>
+) => {
+	return (
+		<ReactSelectIndexed
+			values={props.tokens}
+			onSelect={props.onTokenSelected}
+			onDeselect={props.onTokenDeselected}
+			render={renderTokenSelectorOption}
+			filter={textSearchTokenFormat}
+			theme={reactSelectBlackTheme}
+			selectedIdx={props.tokenSelectedIdx}
+			disabled={props.disabled}
+		/>
+	)
+}
+
