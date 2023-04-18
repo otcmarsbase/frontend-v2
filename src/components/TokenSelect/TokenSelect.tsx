4ac60915b01112da913754f67e38bede1d6a8e67
dmi <dmi.villione@gmail.com>
2023-04-18 15:29:41 +0300

    add dummy token select

---
+++ src/components/TokenSelect/TokenSelect.tsx
@@ -0,0 +1,14 @@
+import { TokenInfo } from '@/types'
+import React from 'react'
+
+
+export type TokenGroup = {
+	name: string
+	tokens: TokenInfo[]
+}
+
+type TokenSelectProps = {}
+
+export const TokenSelect: React.FC<TokenSelectProps> = ({}) => {
+    return <div></div>
+}
