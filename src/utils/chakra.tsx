2a7c283c67177b7fc76e7ac91032b156165858c7
dmi <dmi.villione@gmail.com>
2023-04-18 15:29:41 +0300

    add chakra colors

--- src/utils/chakra.tsx
+++ src/utils/chakra.tsx
@@ -18,6 +18,21 @@ export const queries = {
 export const theme = extendTheme({
 	breakpoints,
 	config,
+	colors: {
+		dark: {
+			50: "#94969a",
+			100: "#686a6e",
+			200: "#686a6e",
+			300: "#515460",
+			400: "#24252d",
+			500: "#2a2b32",
+			600: "#2c2c2e",
+			700: "#2a2a2c",
+			800: "#1b1b1c",
+			900: "#0b0b0b",
+			950: "#000000",
+		},
+	},
 	styles: {
 		global: () => ({
 			// TODO: переделать
