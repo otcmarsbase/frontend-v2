13a3b71dc1583892635c7d67bf54ac20aaa6c877
dmi <dmi.villione@gmail.com>
2023-04-18 15:29:41 +0300

    add currency icon content

--- src/components/CurrencyIcon/CurrencyIcon.tsx
+++ src/components/CurrencyIcon/CurrencyIcon.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { Image } from "@/components/Image/Image"
+import { BaseText } from "@/components/Text/BaseText"
+import { Flex } from "@chakra-ui/react"
+import React from "react"
 
-type CurrencyIconProps = {}
+type CurrencyIconProps = {
+	imgSrc: string
+}
 
-export const CurrencyIcon: React.FC<CurrencyIconProps> = ({}) => {
-    return <div></div>
+export const CurrencyIcon: React.FCC<CurrencyIconProps> = ({
+	imgSrc,
+	children,
+}) => {
+	return (
+		<Flex className="items-center gap-2">
+			<Image size="m" src={imgSrc} />
+			{children && <BaseText>{children}</BaseText>}
+		</Flex>
+	)
 }
