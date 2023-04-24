0161200287dbe1572171de817c48c28f7fd5cc12
dmi <dmi.villione@gmail.com>
2023-04-24 12:00:28 +0300

    upd SpinnerWithText texts

--- src/components/SpinnerWithText.tsx
+++ src/components/SpinnerWithText.tsx
@@ -1,4 +1,4 @@
-import { Text } from "@/components/Text/Text"
+import { LeadText } from "@/components/Text/Typography"
 import { Flex, Spinner, VStack } from "@chakra-ui/react"
 
 export const SpinnerWithText: React.FCC<{ height?: string }> = ({
@@ -8,7 +8,7 @@ export const SpinnerWithText: React.FCC<{ height?: string }> = ({
 	<Flex justify="center" align="center" minH={height || "100vh"}>
 		<VStack>
 			<Spinner size="xl" />
-			<Text>{children}</Text>
+			<LeadText>{children}</LeadText>
 		</VStack>
 	</Flex>
 )
