3e696b9c7c5fa111d405c459f3d546f3c26720b9
dmi <dmi.villione@gmail.com>
2023-04-24 11:59:17 +0300

    upd WelcomePopup texts

--- src/components/WelcomePopup/WelcomePopup.tsx
+++ src/components/WelcomePopup/WelcomePopup.tsx
@@ -1,6 +1,6 @@
 import { GradientPopup } from "@/components/Popup/Popup"
-import { Text } from "@/components/Text/Text"
-import { Box, Button, HStack, VStack } from "@chakra-ui/react"
+import { H2, LeadText } from "@/components/Text/Typography"
+import { Box, Button, VStack } from "@chakra-ui/react"
 import React from "react"
 
 type WelcomePopupProps = {
@@ -11,11 +11,11 @@ export const WelcomePopup: React.FC<WelcomePopupProps> = ({ onClose }) => {
 	return (
 		<GradientPopup contentClassName="max-w-[436px] bg-white ">
 			<VStack className="text-center">
-				<Text>Welcome to DeFi OTC Marsbase</Text>
-				<Text>
+				<H2>Welcome to DeFi OTC Marsbase</H2>
+				<LeadText>
 					Connect to one of the available providers or create a new
 					wallet
-				</Text>
+				</LeadText>
 				<Button>WATCH ABOUT US</Button>
 				<Box>Metamask</Box>
 			</VStack>
