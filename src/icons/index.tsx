00a91f430cb27ef5dcb35d76b03a0822dea85571
dmi <dmi.villione@gmail.com>
2023-04-25 19:43:55 +0300

    regenerate icons

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -3,13 +3,18 @@
 // To regenerate this file run: yarn generate-images
 
 import { ReactComponent as ArrowsIconSource } from "@/assets/icon/arrows.svg"
+import { ReactComponent as BagIconSource } from "@/assets/icon/bag.svg"
+import { ReactComponent as BidsIconSource } from "@/assets/icon/bids.svg"
+import { ReactComponent as ClockIconSource } from "@/assets/icon/clock.svg"
 import { ReactComponent as DashboardIconSource } from "@/assets/icon/dashboard.svg"
 import { ReactComponent as DiscordIconSource } from "@/assets/icon/discord.svg"
 import { ReactComponent as FilterIconSource } from "@/assets/icon/filter.svg"
 import { ReactComponent as GoBackIconSource } from "@/assets/icon/goBack.svg"
 import { ReactComponent as InfoIconSource } from "@/assets/icon/info.svg"
+import { ReactComponent as LightningIconSource } from "@/assets/icon/lightning.svg"
 import { ReactComponent as LinktreeIconSource } from "@/assets/icon/linktree.svg"
 import { ReactComponent as MediumIconSource } from "@/assets/icon/medium.svg"
+import { ReactComponent as PercentIconSource } from "@/assets/icon/percent.svg"
 import { ReactComponent as QuestionIconSource } from "@/assets/icon/question.svg"
 import { ReactComponent as RedditIconSource } from "@/assets/icon/reddit.svg"
 import { ReactComponent as RightArrowIconSource } from "@/assets/icon/rightArrow.svg"
@@ -32,6 +37,12 @@ const img = (img: any) => {
 
 export const ArrowsIcon = img(ArrowsIconSource)
 
+export const BagIcon = img(BagIconSource)
+
+export const BidsIcon = img(BidsIconSource)
+
+export const ClockIcon = img(ClockIconSource)
+
 export const DashboardIcon = img(DashboardIconSource)
 
 export const DiscordIcon = img(DiscordIconSource)
@@ -42,9 +53,13 @@ export const GoBackIcon = img(GoBackIconSource)
 
 export const InfoIcon = img(InfoIconSource)
 
+export const LightningIcon = img(LightningIconSource)
+
 export const LinktreeIcon = img(LinktreeIconSource)
 
 export const MediumIcon = img(MediumIconSource)
+
+export const PercentIcon = img(PercentIconSource)
 
 export const QuestionIcon = img(QuestionIconSource)
 
