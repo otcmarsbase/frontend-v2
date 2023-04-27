ddbdef41d2900eed92609b82ce502ba284bba3a3
dmi <dmi.villione@gmail.com>
2023-04-27 15:40:03 +0300

    add buton spinner

--- src/components/Button/BaseButton.tsx
+++ src/components/Button/BaseButton.tsx
@@ -1,8 +1,25 @@
 import React from "react"
-import { Button, ButtonProps } from "@chakra-ui/react"
+import { Button, ButtonProps, Spinner } from "@chakra-ui/react"
 
 type BaseButtonProps = ButtonProps & {}
 
 export const BaseButton: React.FCC<BaseButtonProps> = ({ children }) => {
-	return <Button>{children}</Button>
+	return (
+		<Button
+			style={{
+				height: "48px",
+				width: "100%",
+			}}
+			spinner={
+				<Spinner
+					thickness="2px"
+					emptyColor="#515460"
+					color="#C74A26"
+					speed="1s"
+				/>
+			}
+		>
+			{children}
+		</Button>
+	)
 }
