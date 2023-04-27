60450af86a303e136c38d98fd41abbb59de96424
dmi <dmi.villione@gmail.com>
2023-04-27 13:00:03 +0300

    add button props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react"
 
-type ButtonProps = {}
+type ButtonProps = {
+	onClick: (ev: React.MouseEvent<HTMLButtonElement>) => void
+	loading?: boolean
+	disabled?: boolean
+}
 
 export const Button: React.FC<ButtonProps> = ({}) => {
-    return <div></div>
+	return <div></div>
 }
