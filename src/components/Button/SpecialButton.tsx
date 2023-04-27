3bafa2a930a1af4d1fbf4759e6efe8cb51059860
dmi <dmi.villione@gmail.com>
2023-04-27 20:10:53 +0300

    add dummy specialbutton

---
+++ src/components/Button/SpecialButton.tsx
@@ -0,0 +1,8 @@
+import { BaseButtonProps } from "@/components/Button/BaseButton"
+import React from "react"
+
+type SpecialButtonProps = {} & BaseButtonProps
+
+export const SpecialButton: React.FC<SpecialButtonProps> = ({}) => {
+	return <div></div>
+}
