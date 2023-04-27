d6349d7f79e4dce2d89ef0c93a4fd8a5de883903
dmi <dmi.villione@gmail.com>
2023-04-27 20:39:44 +0300

    primary button add animations & styles

--- src/components/Button/PrimaryButton.tsx
+++ src/components/Button/PrimaryButton.tsx
@@ -1,8 +1,20 @@
-import { BaseButtonProps } from '@/components/Button/BaseButton'
-import React from 'react'
+import { BaseButton, BaseButtonProps } from "@/components/Button/BaseButton"
+import React from "react"
 
 type PrimaryButtonProps = {} & BaseButtonProps
 
-export const PrimaryButton: React.FC<PrimaryButtonProps> = ({}) => {
-    return <div></div>
+export const PrimaryButton: React.FCC<PrimaryButtonProps> = ({ children }) => {
+	return (
+		<BaseButton
+			bg={"gradientBrightOrange"}
+			_active={{
+				bg: "gradientBrightOrange",
+				transform: `scale(0.98)`,
+			}}
+			_hover={{ bg: "gradientBrightOrange", opacity: 0.8 }}
+			transition={"all 0.3s ease"}
+		>
+			{children}
+		</BaseButton>
+	)
 }
