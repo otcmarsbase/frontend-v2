9ea23a5c26ca98a53ff30e619d044165a7fc49b5
dmi <dmi.villione@gmail.com>
2023-04-27 20:58:02 +0300

    primary button add loading props

--- src/components/Button/PrimaryButton.tsx
+++ src/components/Button/PrimaryButton.tsx
@@ -17,6 +17,7 @@ export const PrimaryButton: React.FCC<PrimaryButtonProps> = ({
 			_hover={{ bg: "gradientBrightOrange", opacity: 0.8 }}
 			transition={"all 0.3s ease"}
 			_disabled={{ opacity: 0.2, bg: "dark.200" }}
+			_loading={{ bg: "rgba(104, 106, 110, 0.4)", opacity: 1 }}
 			{...props}
 		>
 			{children}
