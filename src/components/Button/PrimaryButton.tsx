78d3df1be2406b9fabb89ab83bdb8125c5a85adc
dmi <dmi.villione@gmail.com>
2023-04-27 20:10:35 +0300

    add dummy primarybutton

---
+++ src/components/Button/PrimaryButton.tsx
@@ -0,0 +1,8 @@
+import { BaseButtonProps } from '@/components/Button/BaseButton'
+import React from 'react'
+
+type PrimaryButtonProps = {} & BaseButtonProps
+
+export const PrimaryButton: React.FC<PrimaryButtonProps> = ({}) => {
+    return <div></div>
+}
