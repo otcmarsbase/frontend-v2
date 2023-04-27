684fe2a13dbd449fe4d5d9d7eb34ad982028a2d0
dmi <dmi.villione@gmail.com>
2023-04-27 20:10:42 +0300

    add dummy secondarybutton

---
+++ src/components/Button/SecondaryButton.tsx
@@ -0,0 +1,8 @@
+import { BaseButtonProps } from '@/components/Button/BaseButton'
+import React from 'react'
+
+type SecondaryButtonProps = {} & BaseButtonProps
+
+export const SecondaryButton: React.FC<SecondaryButtonProps> = ({}) => {
+    return <div></div>
+}
