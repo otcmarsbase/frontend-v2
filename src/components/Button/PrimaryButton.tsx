55734f5392977ef8657153e7302ae4faad08db29
dmi <dmi.villione@gmail.com>
2023-05-02 20:47:21 +0300

    [primary button] remove memo

--- src/components/Button/PrimaryButton.tsx
+++ src/components/Button/PrimaryButton.tsx
@@ -8,33 +8,25 @@ export const PrimaryButton: React.FCC<PrimaryButtonProps> = ({
 	children,
 	...props
 }) => {
-	const styles = React.useMemo(() => {
-		const obj: BaseButtonProps = {
-			_active: {
-				bg: "gradientBrightOrange",
-				transform: `scale(0.98)`,
-			},
-			_hover: {
-				bg: "gradientBrightOrange",
-				opacity: 0.8,
-			},
-			_disabled: {
-				opacity: 0.2,
-				bg: "dark.200",
-			},
-			_loading: {
-				bg: "rgba(104, 106, 110, 0.4)",
-				opacity: 1,
-			},
-		}
-		return obj
-	}, [])
-
 	return (
 		<BaseButton
 			bg={"gradientBrightOrange"}
 			transition={"all 0.3s ease"}
-			{...styles}
+			_active={{
+				bg: "gradientBrightOrange",
+			}}
+			_hover={{
+				bg: "gradientBrightOrange",
+				opacity: 0.8,
+			}}
+			_disabled={{
+				opacity: 0.2,
+				bg: "dark.200",
+			}}
+			_loading={{
+				bg: "rgba(104, 106, 110, 0.4)",
+				opacity: 1,
+			}}
 			{...props}
 		>
 			{children}
