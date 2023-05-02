a034b3ebd4e59cec895a8252c08491607aea8741
dmi <dmi.villione@gmail.com>
2023-05-02 20:47:21 +0300

    memo styles

--- src/components/Button/PrimaryButton.tsx
+++ src/components/Button/PrimaryButton.tsx
@@ -1,4 +1,5 @@
 import { BaseButton, BaseButtonProps } from "@/components/Button/BaseButton"
+import { SystemStyleObject } from "@chakra-ui/react"
 import React from "react"
 
 type PrimaryButtonProps = {} & BaseButtonProps
@@ -7,17 +8,33 @@ export const PrimaryButton: React.FCC<PrimaryButtonProps> = ({
 	children,
 	...props
 }) => {
+	const styles = React.useMemo(() => {
+		const obj: BaseButtonProps = {
+			_active: {
+				bg: "gradientBrightOrange",
+				transform: `scale(0.98)`,
+			},
+			_hover: {
+				bg: "gradientBrightOrange",
+				opacity: 0.8,
+			},
+			_disabled: {
+				opacity: 0.2,
+				bg: "dark.200",
+			},
+			_loading: {
+				bg: "rgba(104, 106, 110, 0.4)",
+				opacity: 1,
+			},
+		}
+		return obj
+	}, [])
+
 	return (
 		<BaseButton
 			bg={"gradientBrightOrange"}
-			_active={{
-				bg: "gradientBrightOrange",
-				transform: `scale(0.98)`,
-			}}
-			_hover={{ bg: "gradientBrightOrange", opacity: 0.8 }}
 			transition={"all 0.3s ease"}
-			_disabled={{ opacity: 0.2, bg: "dark.200" }}
-			_loading={{ bg: "rgba(104, 106, 110, 0.4)", opacity: 1 }}
+			{...styles}
 			{...props}
 		>
 			{children}
