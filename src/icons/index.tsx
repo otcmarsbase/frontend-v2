104763d455d6c35748b4327dc053d7edeec63391
dmi <dmi.villione@gmail.com>
2023-05-02 20:47:21 +0300

    regenerate icons

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -4,28 +4,72 @@
  * To regenerate this file run: yarn generate-images
  */
 
-import { ReactComponent as ArrowsIconSource } from "@/assets/icon/arrows.svg"
-import { ReactComponent as BagIconSource } from "@/assets/icon/bag.svg"
-import { ReactComponent as BidsIconSource } from "@/assets/icon/bids.svg"
-import { ReactComponent as ClockIconSource } from "@/assets/icon/clock.svg"
-import { ReactComponent as DashboardIconSource } from "@/assets/icon/dashboard.svg"
-import { ReactComponent as DiscordIconSource } from "@/assets/icon/discord.svg"
-import { ReactComponent as FilterIconSource } from "@/assets/icon/filter.svg"
-import { ReactComponent as GoBackIconSource } from "@/assets/icon/goBack.svg"
-import { ReactComponent as InfoIconSource } from "@/assets/icon/info.svg"
-import { ReactComponent as LightningIconSource } from "@/assets/icon/lightning.svg"
-import { ReactComponent as LinktreeIconSource } from "@/assets/icon/linktree.svg"
-import { ReactComponent as LoaderIconSource } from "@/assets/icon/loader.svg"
-import { ReactComponent as MediumIconSource } from "@/assets/icon/medium.svg"
-import { ReactComponent as PercentIconSource } from "@/assets/icon/percent.svg"
-import { ReactComponent as QuestionIconSource } from "@/assets/icon/question.svg"
-import { ReactComponent as RedditIconSource } from "@/assets/icon/reddit.svg"
-import { ReactComponent as RightArrowIconSource } from "@/assets/icon/rightArrow.svg"
-import { ReactComponent as SearchIconSource } from "@/assets/icon/search.svg"
-import { ReactComponent as SwapHorizontalIconSource } from "@/assets/icon/swapHorizontal.svg"
-import { ReactComponent as SwapVerticalIconSource } from "@/assets/icon/swapVertical.svg"
-import { ReactComponent as TelegramIconSource } from "@/assets/icon/telegram.svg"
-import { ReactComponent as TwitterIconSource } from "@/assets/icon/twitter.svg"
+import ArrowsIconSourceRaw, {
+    ReactComponent as ArrowsIconSource,
+} from "@/assets/icon/arrows.svg"
+import BagIconSourceRaw, {
+    ReactComponent as BagIconSource,
+} from "@/assets/icon/bag.svg"
+import BidsIconSourceRaw, {
+    ReactComponent as BidsIconSource,
+} from "@/assets/icon/bids.svg"
+import ClockIconSourceRaw, {
+    ReactComponent as ClockIconSource,
+} from "@/assets/icon/clock.svg"
+import DashboardIconSourceRaw, {
+    ReactComponent as DashboardIconSource,
+} from "@/assets/icon/dashboard.svg"
+import DiscordIconSourceRaw, {
+    ReactComponent as DiscordIconSource,
+} from "@/assets/icon/discord.svg"
+import FilterIconSourceRaw, {
+    ReactComponent as FilterIconSource,
+} from "@/assets/icon/filter.svg"
+import GoBackIconSourceRaw, {
+    ReactComponent as GoBackIconSource,
+} from "@/assets/icon/goBack.svg"
+import InfoIconSourceRaw, {
+    ReactComponent as InfoIconSource,
+} from "@/assets/icon/info.svg"
+import LightningIconSourceRaw, {
+    ReactComponent as LightningIconSource,
+} from "@/assets/icon/lightning.svg"
+import LinktreeIconSourceRaw, {
+    ReactComponent as LinktreeIconSource,
+} from "@/assets/icon/linktree.svg"
+import LoaderIconSourceRaw, {
+    ReactComponent as LoaderIconSource,
+} from "@/assets/icon/loader.svg"
+import MediumIconSourceRaw, {
+    ReactComponent as MediumIconSource,
+} from "@/assets/icon/medium.svg"
+import PercentIconSourceRaw, {
+    ReactComponent as PercentIconSource,
+} from "@/assets/icon/percent.svg"
+import QuestionIconSourceRaw, {
+    ReactComponent as QuestionIconSource,
+} from "@/assets/icon/question.svg"
+import RedditIconSourceRaw, {
+    ReactComponent as RedditIconSource,
+} from "@/assets/icon/reddit.svg"
+import RightArrowIconSourceRaw, {
+    ReactComponent as RightArrowIconSource,
+} from "@/assets/icon/rightArrow.svg"
+import SearchIconSourceRaw, {
+    ReactComponent as SearchIconSource,
+} from "@/assets/icon/search.svg"
+import SwapHorizontalIconSourceRaw, {
+    ReactComponent as SwapHorizontalIconSource,
+} from "@/assets/icon/swapHorizontal.svg"
+import SwapVerticalIconSourceRaw, {
+    ReactComponent as SwapVerticalIconSource,
+} from "@/assets/icon/swapVertical.svg"
+import TelegramIconSourceRaw, {
+    ReactComponent as TelegramIconSource,
+} from "@/assets/icon/telegram.svg"
+import TwitterIconSourceRaw, {
+    ReactComponent as TwitterIconSource,
+} from "@/assets/icon/twitter.svg"
 import React from "react"
 import { Icon as ChakraIcon } from "@chakra-ui/react"
 
@@ -40,44 +84,88 @@ const img = (img: any) => {
 
 export const ArrowsIcon = img(ArrowsIconSource)
 
+export const ArrowsIconRaw = ArrowsIconSourceRaw
+
 export const BagIcon = img(BagIconSource)
+
+export const BagIconRaw = BagIconSourceRaw
 
 export const BidsIcon = img(BidsIconSource)
 
+export const BidsIconRaw = BidsIconSourceRaw
+
 export const ClockIcon = img(ClockIconSource)
+
+export const ClockIconRaw = ClockIconSourceRaw
 
 export const DashboardIcon = img(DashboardIconSource)
 
+export const DashboardIconRaw = DashboardIconSourceRaw
+
 export const DiscordIcon = img(DiscordIconSource)
+
+export const DiscordIconRaw = DiscordIconSourceRaw
 
 export const FilterIcon = img(FilterIconSource)
 
+export const FilterIconRaw = FilterIconSourceRaw
+
 export const GoBackIcon = img(GoBackIconSource)
+
+export const GoBackIconRaw = GoBackIconSourceRaw
 
 export const InfoIcon = img(InfoIconSource)
 
+export const InfoIconRaw = InfoIconSourceRaw
+
 export const LightningIcon = img(LightningIconSource)
+
+export const LightningIconRaw = LightningIconSourceRaw
 
 export const LinktreeIcon = img(LinktreeIconSource)
 
+export const LinktreeIconRaw = LinktreeIconSourceRaw
+
 export const LoaderIcon = img(LoaderIconSource)
+
+export const LoaderIconRaw = LoaderIconSourceRaw
 
 export const MediumIcon = img(MediumIconSource)
 
+export const MediumIconRaw = MediumIconSourceRaw
+
 export const PercentIcon = img(PercentIconSource)
+
+export const PercentIconRaw = PercentIconSourceRaw
 
 export const QuestionIcon = img(QuestionIconSource)
 
+export const QuestionIconRaw = QuestionIconSourceRaw
+
 export const RedditIcon = img(RedditIconSource)
+
+export const RedditIconRaw = RedditIconSourceRaw
 
 export const RightArrowIcon = img(RightArrowIconSource)
 
+export const RightArrowIconRaw = RightArrowIconSourceRaw
+
 export const SearchIcon = img(SearchIconSource)
+
+export const SearchIconRaw = SearchIconSourceRaw
 
 export const SwapHorizontalIcon = img(SwapHorizontalIconSource)
 
+export const SwapHorizontalIconRaw = SwapHorizontalIconSourceRaw
+
 export const SwapVerticalIcon = img(SwapVerticalIconSource)
+
+export const SwapVerticalIconRaw = SwapVerticalIconSourceRaw
 
 export const TelegramIcon = img(TelegramIconSource)
 
+export const TelegramIconRaw = TelegramIconSourceRaw
+
 export const TwitterIcon = img(TwitterIconSource)
+
+export const TwitterIconRaw = TwitterIconSourceRaw
