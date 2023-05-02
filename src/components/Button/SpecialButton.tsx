d5339a845f1c637eec33e50d1840477f20127242
dmi <dmi.villione@gmail.com>
2023-05-02 20:47:21 +0300

    [special button] remoe memo

--- src/components/Button/SpecialButton.tsx
+++ src/components/Button/SpecialButton.tsx
@@ -1,8 +1,34 @@
-import { BaseButtonProps } from "@/components/Button/BaseButton"
+import { BaseButton, BaseButtonProps } from "@/components/Button/BaseButton"
 import React from "react"
 
 type SpecialButtonProps = {} & BaseButtonProps
 
-export const SpecialButton: React.FC<SpecialButtonProps> = ({}) => {
-	return <div></div>
+export const SpecialButton: React.FC<SpecialButtonProps> = ({
+	children,
+	...props
+}) => {
+	return (
+		<BaseButton
+			bg={"specialBtnGradient"}
+			transition={"all 0.3s ease"}
+			_active={{
+				bg: "specialBtnGradient",
+			}}
+			_hover={{
+				bg: "specialBtnGradient",
+				opacity: 0.8,
+			}}
+			_disabled={{
+				opacity: 0.2,
+				bg: "specialBtnGradient",
+			}}
+			_loading={{
+				bg: "rgba(104, 106, 110, 0.4)",
+				opacity: 1,
+			}}
+			{...props}
+		>
+			{children}
+		</BaseButton>
+	)
 }
