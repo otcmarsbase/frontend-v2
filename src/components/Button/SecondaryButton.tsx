72f5d4ffc874691d982e8778b482553c25193527
dmi <dmi.villione@gmail.com>
2023-05-03 13:38:37 +0300

    add secondaty button

--- src/components/Button/SecondaryButton.tsx
+++ src/components/Button/SecondaryButton.tsx
@@ -1,8 +1,31 @@
-import { BaseButtonProps } from '@/components/Button/BaseButton'
-import React from 'react'
+import { BaseButton, BaseButtonProps } from "@/components/Button/BaseButton"
+import React from "react"
 
 type SecondaryButtonProps = {} & BaseButtonProps
 
-export const SecondaryButton: React.FC<SecondaryButtonProps> = ({}) => {
-    return <div></div>
+export const SecondaryButton: React.FCC<SecondaryButtonProps> = ({
+	children,
+	...props
+}) => {
+	return (
+		<BaseButton
+			_active={{
+				bg: "transparent",
+			}}
+			_hover={{
+				
+				bg: "transparent",
+			}}
+			_disabled={{
+				bg: "transparent",
+			}}
+			_loading={{
+				bg: "transparent",
+				opacity: 1,
+			}}
+			{...props}
+		>
+			{children}
+		</BaseButton>
+	)
 }
