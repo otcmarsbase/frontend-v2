4bfed5375a6b898cb0291e402a1839260d5f061b
dmi <dmi.villione@gmail.com>
2023-05-03 13:38:37 +0300

    [SecondaryButton] reset border styles on disabled & loading states

--- src/components/Button/SecondaryButton.tsx
+++ src/components/Button/SecondaryButton.tsx
@@ -17,13 +17,17 @@ export const SecondaryButton: React.FCC<SecondaryButtonProps> = ({
 			}}
 			_hover={{
 				borderColor: "orange.500",
-                color: "orange.500",
+				color: "orange.500",
 				bg: "transparent",
 			}}
 			_disabled={{
+				borderColor: "dark.300",
+				color: "unset",
 				bg: "transparent",
 			}}
 			_loading={{
+				borderColor: "dark.300",
+				color: "unset",
 				bg: "transparent",
 				opacity: 1,
 			}}
