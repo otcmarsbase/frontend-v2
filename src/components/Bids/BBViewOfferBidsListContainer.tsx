617ce43c27ab066d79875f5f909ff4f01ffe2e80
dmi <dmi.villione@gmail.com>
2023-05-05 19:50:17 +0300

    add BBViewOfferBidsListContainer

---
+++ src/components/Bids/BBViewOfferBidsListContainer.tsx
@@ -0,0 +1,15 @@
+import { ViewOfferBidsTable } from "@/components/Bids/ViewOfferBidsTable"
+import { SpecialButton } from "@/components/Button/SpecialButton"
+import { FormControlHeader } from "@/components/FormControlHeader/FormControlHeader"
+import { Flex } from "@chakra-ui/react"
+import React from "react"
+
+type BBViewOfferBidsListContainerProps = {}
+
+export const BBViewOfferBidsListContainer: React.FC<
+	BBViewOfferBidsListContainerProps
+> = ({}) => {
+	return (
+		
+	)
+}
