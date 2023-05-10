aa8cdb2fbd965f24a7714b48ad2b028f26ff5193
dmi <dmi.villione@gmail.com>
2023-05-10 10:41:36 +0300

    add FinishedCreated step

--- src/components/CreateOffer/CreateOfferDetails/CreateOfferDetails.tsx
+++ src/components/CreateOffer/CreateOfferDetails/CreateOfferDetails.tsx
@@ -213,6 +213,21 @@ export const LoadingCreated: React.FC = (props) => {
 	)
 }
 
+export const FinishedCreated: React.FC = (props) => {
+	return (
+		<Flex direction={"column"} textAlign="center" alignItems={"center"}>
+			<AnimatedProgress status={"success"} />
+			<H3>{"Order #123 was created"}</H3>
+			<Text12Normal color="grey">
+				You successfully left your mark on Mars. Wanna make another one?
+			</Text12Normal>
+			<Flex>
+				<SecondaryButton onClick={() => {}}>Go to OTC</SecondaryButton>
+				<PrimaryButton onClick={() => {}}>Try again</PrimaryButton>
+			</Flex>
+		</Flex>
+	)
+}
 const DetailsItem: React.FC<{
 	title: React.ReactNode
 	subTitle?: React.ReactNode
