dadb42a9bc9690f6916b9e74258e5b51725e9e9e
dmi <dmi.villione@gmail.com>
2023-05-12 21:17:44 +0300

    [TokenApproximatelyEquals] remove useless styles

--- src/components/TokenApproximatelyEquals.tsx
+++ src/components/TokenApproximatelyEquals.tsx
@@ -23,8 +23,6 @@ export const TokenApproximatelyEquals: React.FCC<
 	let style = useMemo(
 		(): React.CSSProperties => ({
 			gap: props.size == "big" ? "1em" : "0.5em",
-			fontSize: props.size == "big" ? "1.5em" : "1em",
-			fontWeight: props.size == "big" ? "bold" : "inherit",
 		}),
 		[props.size]
 	)
