63a617112f72fdae8a1b2641446e426ebb9dc2dc
dmi <dmi.villione@gmail.com>
2023-05-15 19:57:21 +0300

    [MPViewOfferHydrator] add content

--- src/components/ViewOffer/MPViewOfferHydrator.tsx
+++ src/components/ViewOffer/MPViewOfferHydrator.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { MPViewOfferWrapper } from "@/components/ViewOffer/ViewOfferWrapper"
+import React from "react"
 
 type MPViewOfferHydratorProps = {}
 
 export const MPViewOfferHydrator: React.FC<MPViewOfferHydratorProps> = ({}) => {
-    return <div></div>
+	const offerId = 0
+	const error = null
+	if (false) return <div>Loading offer #{offerId}...</div>
+
+	if (false)
+		return (
+			<div>
+				Error loading offer #{offerId}! {typeof error}
+				<br />
+				{JSON.stringify(error)}
+			</div>
+		)
+
+	// if (false)
+	// 	return <ViewOfferNotFound offerId={offerId} />
+
+	return <MPViewOfferWrapper />
 }
