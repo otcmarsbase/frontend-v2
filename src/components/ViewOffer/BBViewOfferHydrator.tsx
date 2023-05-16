2f45905b6a319b627ff6fab267cf5e642a1bcc42
dmi <dmi.villione@gmail.com>
2023-05-16 11:09:42 +0300

    [BBViewOfferHydrator] add views condition

--- src/components/ViewOffer/BBViewOfferHydrator.tsx
+++ src/components/ViewOffer/BBViewOfferHydrator.tsx
@@ -1,24 +1,14 @@
+import { BBViewOfferBidsList } from "@/components/ViewOffer/BBViewOfferBidsList"
+import { BBViewOfferCreateBid } from "@/components/ViewOffer/BBViewOfferCreateBid"
 import { BBViewOfferWrapper } from "@/components/ViewOffer/ViewOfferWrapper"
 import React from "react"
 
 type BBViewOfferHydratorProps = {}
 
 export const BBViewOfferHydrator: React.FC<BBViewOfferHydratorProps> = ({}) => {
-	const offerId = 0
-	const error = null
-	if (false) return <div>Loading offer #{offerId}...</div>
-
-	if (false)
-		return (
-			<div>
-				Error loading offer #{offerId}! {typeof error}
-				<br />
-				{JSON.stringify(error)}
-			</div>
-		)
-
-	// if (false)
-	// 	return <ViewOfferNotFound offerId={offerId} />
-
-	return <BBViewOfferWrapper />
+	return (
+		<BBViewOfferWrapper>
+			{true ? <BBViewOfferCreateBid /> : <BBViewOfferBidsList />}
+		</BBViewOfferWrapper>
+	)
 }
