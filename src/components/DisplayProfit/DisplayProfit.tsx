10bed235e4bdd15fa59c3fe5929b72362da0efb9
dmi <dmi.villione@gmail.com>
2023-05-16 11:09:42 +0300

    add DisplayProfit

---
+++ src/components/DisplayProfit/DisplayProfit.tsx
@@ -0,0 +1,23 @@
+import { Text12Bold, Text12Normal } from "@/components/Text/Typography"
+import { calculateProfit } from "@/utils/utils"
+import { Flex } from "@chakra-ui/react"
+import clsx from "clsx"
+
+export const DisplayProfit: React.FC<{
+	profit: ReturnType<typeof calculateProfit>
+}> = ({ profit }) => {
+	return (
+		<Flex>
+			<Text12Bold
+				size="12"
+				className={clsx({
+					"text-red-300": profit.type === "discount",
+					"text-green-200": profit.type === "premium",
+				})}
+			>
+				{(profit.amount * 100).toFixed(2)}%
+			</Text12Bold>
+			<Text12Normal>{profit.type}</Text12Normal>
+		</Flex>
+	)
+}
