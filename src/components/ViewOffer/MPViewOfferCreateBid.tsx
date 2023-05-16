1e5f30e927b33fadb86c0bc5c09a5f29fc227a1b
dmi <dmi.villione@gmail.com>
2023-05-16 11:09:42 +0300

    add MPViewOfferCreateBid

---
+++ src/components/ViewOffer/MPViewOfferCreateBid.tsx
@@ -0,0 +1,44 @@
+import { ViewOfferBidCreateForm } from "@/components/ViewOffer/ViewOfferBidCreateForm"
+import React from "react"
+
+type MPViewOfferCreateBidProps = {}
+
+export const MPViewOfferCreateBid: React.FC<
+	MPViewOfferCreateBidProps
+> = ({}) => {
+	return (
+		<ViewOfferBidCreateForm
+			tokens={[]}
+			tokenSelectedIdx={0}
+			onTokenSelected={function (idx: number): void {
+				throw new Error("Function not implemented.")
+			}}
+			onTokenDeselected={function (): void {
+				throw new Error("Function not implemented.")
+			}}
+			createEnabled={false}
+			onCreate={function (): void {
+				throw new Error("Function not implemented.")
+			}}
+			amountBobInput={""}
+			onAmountBobInput={function (amount: string): void {
+				throw new Error("Function not implemented.")
+			}}
+			balanceBob={undefined}
+			amountBobInputError={undefined}
+			shouldShowLogin={false}
+			onLogin={function (): void {
+				throw new Error("Function not implemented.")
+			}}
+			disableAllInputs={false}
+			transactionInProgress={false}
+			tokensWillBeLocked={false}
+			backButton={{
+				label: "",
+				onClick: function (): void {
+					throw new Error("Function not implemented.")
+				},
+			}}
+		/>
+	)
+}
