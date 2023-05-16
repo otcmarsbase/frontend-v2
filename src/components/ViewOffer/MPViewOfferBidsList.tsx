93fd37aedb861eb53fda159e6b012079eaeddca7
dmi <dmi.villione@gmail.com>
2023-05-16 11:09:42 +0300

    add MPViewOfferBidsList

---
+++ src/components/ViewOffer/MPViewOfferBidsList.tsx
@@ -0,0 +1,8 @@
+import { BBViewOfferBidsListContainer } from "@/components/ViewOffer/BBViewOfferBidsListContainer"
+import React from "react"
+
+type MPViewOfferBidsListProps = {}
+
+export const MPViewOfferBidsList: React.FC<MPViewOfferBidsListProps> = ({}) => {
+	return <BBViewOfferBidsListContainer />
+}
