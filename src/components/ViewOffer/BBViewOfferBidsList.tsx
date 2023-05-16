fe3d8a89df3523e232eefc08df2d086c5e87a0e9
dmi <dmi.villione@gmail.com>
2023-05-16 11:09:42 +0300

    add BBViewOfferBidsList

---
+++ src/components/ViewOffer/BBViewOfferBidsList.tsx
@@ -0,0 +1,8 @@
+import { BBViewOfferBidsListContainer } from "@/components/ViewOffer/BBViewOfferBidsListContainer"
+import React from "react"
+
+type BBViewOfferBidsListProps = {}
+
+export const BBViewOfferBidsList: React.FC<BBViewOfferBidsListProps> = ({}) => {
+	return <BBViewOfferBidsListContainer />
+}
