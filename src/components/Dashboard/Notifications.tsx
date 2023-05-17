97e4305f9ae4ac8f35dfaa9100a46c2541cc1138
dmi <dmi.villione@gmail.com>
2023-05-17 10:43:38 +0300

    add notifications hydartor

---
+++ src/components/Dashboard/Notifications.tsx
@@ -0,0 +1,13 @@
+import { PageHeader } from "@/components/PageHeader/PageHeader"
+import { PageWrapper } from "@/components/PageWrapper/PageWrapper"
+import { Center, Flex } from "@chakra-ui/react"
+import React from "react"
+
+type NotificationsProps = {}
+
+export const NotificationsHydrator: React.FC<NotificationsProps> = ({}) => {
+	// let { subscribedAlready, data } = useNotificationEnabledInfo()
+	let subscribedAlready = false
+	return <div></div>
+}
+
