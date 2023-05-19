89c581248969d6245e5344a471458c706057e60a
dmi <dmi.villione@gmail.com>
2023-05-19 12:35:57 +0300

    fix format

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
-import { theme } from './utils/chakra'
-import { App } from './App'
-import { SWRConfig } from 'swr'
+import React from "react"
+import { createRoot } from "react-dom/client"
+import { ChakraProvider } from "@chakra-ui/react"
+import { theme } from "./utils/chakra"
+import { App } from "./App"
+import { SWRConfig } from "swr"
 import "./globals.scss"
 import "./tailwind.css"
 // import '@otcmarsbase/react-components'
 // import '@otcmarsbase/react-components/dist/index.css'
-const container = document.getElementById('root')
+const container = document.getElementById("root")
 const root = createRoot(container!)
 
 root.render(
