22301f3b6b488921eea47c733670d260d2b3e3fa
dmi <dmi.villione@gmail.com>
2023-05-22 17:26:04 +0300

    [DataGrid] add column type

--- src/components/DataGrid/DataGrid.tsx
+++ src/components/DataGrid/DataGrid.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react"
 
 type DataGridProps = {}
 
 export const DataGrid: React.FC<DataGridProps> = ({}) => {
-    return <div></div>
+	return <div></div>
+}
+
+type Column<Row extends object, Key extends keyof Row> = {
+	name: string
+	width?: string
+	key: Key
 }
