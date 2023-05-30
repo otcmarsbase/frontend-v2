def285d6946b903416132d82a774be9f9a1671fb
dmi <dmi.villione@gmail.com>
2023-05-30 16:50:31 +0300

    add MPOffersListView

---
+++ src/components/OffersList/MPOffersListView.tsx
@@ -0,0 +1,8 @@
+import { MPOffersTable } from "@/components/Marketplace/MPOffersTable"
+import React from "react"
+
+type MPOffersListViewProps = {}
+
+export const MPOffersListView: React.FC<MPOffersListViewProps> = ({}) => {
+	return <MPOffersTable />
+}
