9a8c7716d91cc30b51f32844d0ae8e08223ffbef
dmi <dmi.villione@gmail.com>
2023-05-31 16:45:05 +0300

    extract SmallMyOfferLabel to other file

---
+++ src/components/OffersList/SingleOfferView.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { TokenInfo } from "@/types"
+import { WBN } from "@/utils/WBN"
+import { LongEthValueView } from "@/components/TokenAmountInput/TokenAmountInput"
+import { LeadText } from "@/components/Text/Typography"
+import { Flex } from "@chakra-ui/react"
+import { MyOfferIndicator } from "@/components/OfferTypeIndicator/OfferTypeIndicator"
+import { CurrencyIcon } from "@/components/CurrencyIcon/CurrencyIcon"
+
+export const SmallMyOfferLabel: React.FC = () => (
+	<div style={{ transform: "scale(0.75)", display: "inline-block" }}>
+		<MyOfferIndicator />
+	</div>
+)
