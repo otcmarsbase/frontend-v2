47c7f52790172113785d8d1481c5ff7fa0702d8b
dmi <dmi.villione@gmail.com>
2023-05-31 16:45:05 +0300

    remove offerslist view

--- src/components/OffersList/MPOffersListView.tsx
+++
@@ -1,8 +0,0 @@
-import { OffersListViewTable } from "@/components/OffersList/OffersListViewTable"
-import React from "react"
-
-type MPOffersListViewProps = {}
-
-export const MPOffersListView: React.FC<MPOffersListViewProps> = ({}) => {
-	return <OffersListViewTable />
-}
