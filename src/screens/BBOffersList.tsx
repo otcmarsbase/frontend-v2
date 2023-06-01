c73295253349f7df1b6b9380c88efbb75dfc864c
dmi <dmi.villione@gmail.com>
2023-06-01 22:16:08 +0300

    add BBOffersList screen

--- src/screens/BBOffersList.tsx
+++ src/screens/BBOffersList.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import { BBOffersListHydrator } from "@/components/OffersList/BBOffersListHydrator"
+import { PageHeader } from "@/components/PageHeader/PageHeader"
+import { PageWrapper } from "@/components/PageWrapper/PageWrapper"
+import { useTranslation } from "@/localization/l10n"
+import React from "react"
 
 type BBOffersListProps = {}
 
 export const BBOffersList: React.FC<BBOffersListProps> = ({}) => {
-    return <div>BBOffersList screen</div>
+	const l10n = useTranslation()
+	return (
+		<PageWrapper
+			header={
+				<PageHeader
+					createOfferBtn={{
+						label: "CREATE BEST BIT AUCTION",
+						onClick: () => {},
+					}}
+					subTitle={
+						"Trade tokens in the OTC auction market with the most competitive offers or arrange escrow deals with your own counterparties."
+					}
+					title={"Best Bid Auctions & Escrow"}
+				/>
+			}
+		>
+			<BBOffersListHydrator />
+		</PageWrapper>
+	)
 }
