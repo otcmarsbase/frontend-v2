bf78f8282905ee7e26b9d662177c9ae20abe3325
dmi <dmi.villione@gmail.com>
2023-06-05 10:17:42 +0300

    [TumblerOption] add styles

--- src/components/Tumbler/Tumbler.tsx
+++ src/components/Tumbler/Tumbler.tsx
@@ -19,7 +19,7 @@ export const Tumbler: React.FC<TumblerProps> = (props) => {
 
 const TumblerOption: React.FCC<{}> = (props) => {
 	return (
-		<Flex className="items-center justify-center rounded-md py-2 px-1 text-center">
+		<Flex className="items-center justify-center rounded-md py-2 px-1 text-center w-full">
 			{props.children}
 		</Flex>
 	)
