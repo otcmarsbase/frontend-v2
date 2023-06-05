cc0d24082af6a99bbcaaea78a6c7b25423638a77
dmi <dmi.villione@gmail.com>
2023-06-05 10:17:42 +0300

    add TumblerProps

--- src/components/Tumbler/Tumbler.tsx
+++ src/components/Tumbler/Tumbler.tsx
@@ -1,6 +1,12 @@
 import { Flex } from "@chakra-ui/react"
 import React from "react"
 
+type TumblerProps = {
+	options: string[]
+	selectedIdx: number
+	onSelected: (idx: number) => void
+}
+
 const TumblerOption: React.FCC<{}> = (props) => {
 	return (
 		<Flex className="items-center justify-center rounded-md py-2 px-1 text-center">
