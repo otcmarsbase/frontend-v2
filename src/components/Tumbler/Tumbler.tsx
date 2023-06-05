830d9548795dc48eda8117a7c51d0b1529fc91d9
dmi <dmi.villione@gmail.com>
2023-06-05 10:17:42 +0300

    [Tumbler] add props

--- src/components/Tumbler/Tumbler.tsx
+++ src/components/Tumbler/Tumbler.tsx
@@ -11,13 +11,15 @@ export const Tumbler: React.FC<TumblerProps> = (props) => {
 	return (
 		<Flex w={"full"} className="bg-dark-800 p-1 rounded-md">
 			{props.options.map((option, idx) => (
-				<TumblerOption>{option}</TumblerOption>
+				<TumblerOption active={idx === props.selectedIdx}>
+					{option}
+				</TumblerOption>
 			))}
 		</Flex>
 	)
 }
 
-const TumblerOption: React.FCC<{}> = (props) => {
+const TumblerOption: React.FCC<{ active: boolean }> = (props) => {
 	return (
 		<Flex className="items-center justify-center rounded-md py-2 px-1 text-center w-full cursor-pointer">
 			{props.children}
