4458354fdd97d4e0256d04481f2cb6461bf0b111
dmi <dmi.villione@gmail.com>
2023-06-05 10:17:42 +0300

    add Tumbler

--- src/components/Tumbler/Tumbler.tsx
+++ src/components/Tumbler/Tumbler.tsx
@@ -7,6 +7,16 @@ type TumblerProps = {
 	onSelected: (idx: number) => void
 }
 
+export const Tumbler: React.FC<TumblerProps> = (props) => {
+	return (
+		<Flex>
+			{props.options.map((option, idx) => (
+				<TumblerOption>{option}</TumblerOption>
+			))}
+		</Flex>
+	)
+}
+
 const TumblerOption: React.FCC<{}> = (props) => {
 	return (
 		<Flex className="items-center justify-center rounded-md py-2 px-1 text-center">
