b38d3fa5b0b1de24c7158df6da855daa7ebd8448
dmi <dmi.villione@gmail.com>
2023-06-06 11:17:00 +0300

    add CreateOfferSelectionSecond

--- src/components/CreateOffer/CreateOfferSelection/CreateOfferSelectonSecond.tsx
+++ src/components/CreateOffer/CreateOfferSelection/CreateOfferSelectonSecond.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { InputName } from "@/components/CreateOffer/CreateOfferSelection/CreateOfferSelectonFirst"
+import { FormControlHeader } from "@/components/FormControlHeader/FormControlHeader"
+import { Paper } from "@/components/Paper/Paper"
+import { LeadText } from "@/components/Text/Typography"
+import { Tumbler } from "@/components/Tumbler/Tumbler"
+import { Flex, VStack } from "@chakra-ui/react"
+import React from "react"
 
 type CreateOfferSelectonSecondProps = {}
 
-export const CreateOfferSelectonSecond: React.FC<CreateOfferSelectonSecondProps> = (props) => {
-    return <div></div>
+export const CreateOfferSelectonSecond: React.FC<
+	CreateOfferSelectonSecondProps
+> = (props) => {
+	return (
+		<VStack alignItems={"start"} w={"full"}>
+			<FormControlHeader
+				title={"Parameters"}
+				subtitle="Choose the best strategy for exchanging your funds"
+			/>
+			
+		</VStack>
+	)
 }
