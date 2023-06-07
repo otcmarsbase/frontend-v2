f591916fd7d6bc4fa8e7a80ed4d8792e7ffc850c
dmi <dmi.villione@gmail.com>
2023-06-07 13:30:40 +0300

    add calendar props

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import CalendarComponent from "react-calendar"
 import "./Calendar.scss"
 
-type CalendarProps = {}
+type CalendarProps = {} & React.ComponentProps<typeof CalendarComponent>
 
 export const Calendar: React.FC<CalendarProps> = (props) => {
 	return (
