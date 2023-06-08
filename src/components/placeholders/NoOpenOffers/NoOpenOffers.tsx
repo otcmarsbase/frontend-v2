793cb7123fdf2f304f46eea21c27fa80f4f91559
dmi <dmi.villione@gmail.com>
2023-06-08 13:00:31 +0300

    [NoOpenOffersPlaceholder upd icon

--- src/components/placeholders/NoOpenOffers/NoOpenOffers.tsx
+++ src/components/placeholders/NoOpenOffers/NoOpenOffers.tsx
@@ -1,11 +1,11 @@
 import { Center, VStack } from "@chakra-ui/react"
 import { H3, LeadText } from "@/components/Text/Typography"
-import { NoDealsIconRaw } from "@/icons"
+import { NoOffersIconRaw } from "@/icons"
 
 export const NoOpenOffersPlaceholder: React.FCC = ({ children }) => (
 	<Center mt="5em">
 		<VStack textAlign="center" h="100%">
-			<img src={NoDealsIconRaw} alt="" />
+			<img src={NoOffersIconRaw} alt="" />
 			<H3>No open offers</H3>
 			<VStack gap="16px">
 				<LeadText color="grey">
