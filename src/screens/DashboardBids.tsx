e2330dcbef787cc476dafbb9a0b4efad2b2f2670
dmi <dmi.villione@gmail.com>
2023-06-09 13:56:52 +0300

    add DashboardBids page content

--- src/screens/DashboardBids.tsx
+++ src/screens/DashboardBids.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import { HowToUseBtn } from "@/components/HowToUseBtn/HowToUseBtn"
+import { PageHeader } from "@/components/PageHeader/PageHeader"
+import { PageWrapper } from "@/components/PageWrapper/PageWrapper"
+import { Flex } from "@chakra-ui/react"
+import React from "react"
 
 type DashboardBidsProps = {}
 
 export const DashboardBids: React.FC<DashboardBidsProps> = ({}) => {
-    return <div>DashboardBids screen</div>
+	return (
+		<PageWrapper
+			header={
+				<>
+					<PageHeader
+						subTitle={""}
+						title={"My dashboard"}
+						borderBottom
+						titleLink={<HowToUseBtn />}
+						createOfferBtn={{
+							label: "CREATE NEW",
+							onClick: () => {},
+						}}
+					/>
+				</>
+			}
+		>
+			<Flex grow={1}></Flex>
+		</PageWrapper>
+	)
 }
