659bad5a746c8ea22500077ed7421fd590575139
dmi <dmi.villione@gmail.com>
2023-06-09 13:56:52 +0300

    add dashboard tabs

--- src/components/Dashboard/Tabs.tsx
+++ src/components/Dashboard/Tabs.tsx
@@ -1,11 +1,32 @@
+import { appRoutes } from "@/AppRoutes"
+import { Clickable } from "@/components/Clickable/Clickable"
+import { Text } from "@/components/Text/Text"
 import { Flex } from "@chakra-ui/react"
 import React from "react"
+import { useNavigate } from "react-router-dom"
 
 type DasboardTabsProps = {
 	name: "offers" | "bids"
 }
 
 export const DasboardTabs: React.FC<DasboardTabsProps> = (props) => {
+	const navigate = useNavigate()
+	return (
+		<Flex gap={"52px"}>
+			<Tab
+				title="MY DEALS"
+				onClick={() => navigate(appRoutes["/dashboard/offers"]())}
+				isActive={props.name === "offers"}
+			/>
+			<Tab
+				title="MY BIDS"
+				onClick={() => navigate(appRoutes["/dashboard/bids"]())}
+				isActive={props.name === "bids"}
+			/>
+		</Flex>
+	)
+}
+
 const Tab: React.FC<{
 	title: string
 	onClick: () => void
