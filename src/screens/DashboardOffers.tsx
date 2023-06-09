50073abd143a11532ee9c0d51870ba41c0a6cbc6
dmi <dmi.villione@gmail.com>
2023-06-09 13:56:52 +0300

    [DashboardOffers] add tabs

--- src/screens/DashboardOffers.tsx
+++ src/screens/DashboardOffers.tsx
@@ -1,7 +1,10 @@
 import { DasboardTabs } from "@/components/Dashboard/Tabs"
 import { HowToUseBtn } from "@/components/HowToUseBtn/HowToUseBtn"
-import { PageHeader } from "@/components/PageHeader/PageHeader"
-import { PageWrapper } from "@/components/PageWrapper/PageWrapper"
+import {
+	HeaderBottomLine,
+	PageHeader,
+} from "@/components/PageHeader/PageHeader"
+import { PageContainer, PageWrapper } from "@/components/PageWrapper/PageWrapper"
 import { useTranslation } from "@/localization/l10n"
 import { Flex } from "@chakra-ui/react"
 import React from "react"
@@ -17,15 +20,17 @@ export const DashboardOffers: React.FC<DashboardOffersProps> = ({}) => {
 					<PageHeader
 						subTitle={""}
 						title={"My dashboard"}
-						borderBottom
 						titleLink={<HowToUseBtn />}
 						createOfferBtn={{
 							label: "CREATE NEW",
 							onClick: () => {},
 						}}
-					>
+					></PageHeader>
+					<PageContainer>
 						<DasboardTabs name="offers" />
-					</PageHeader>
+					</PageContainer>
+
+					<HeaderBottomLine />
 				</>
 			}
 		>
