8c67a29748f65523274bdfedd3887710e195649b
dmi <dmi.villione@gmail.com>
2023-06-09 13:56:52 +0300

    add DashboardOffers

--- src/screens/DashboardOffers.tsx
+++ src/screens/DashboardOffers.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import { HowToUseBtn } from "@/components/HowToUseBtn/HowToUseBtn"
+import { PageHeader } from "@/components/PageHeader/PageHeader"
+import { PageWrapper } from "@/components/PageWrapper/PageWrapper"
+import { useTranslation } from "@/localization/l10n"
+import { openExternalUrl } from "@/utils/utils"
+import React from "react"
 
 type DashboardOffersProps = {}
 
 export const DashboardOffers: React.FC<DashboardOffersProps> = ({}) => {
-	return <div>DashboardOffers screen</div>
+	const l10n = useTranslation()
+	return (
+		<PageWrapper
+			header={
+				<>
+					<PageHeader
+						subTitle={""}
+						title={"My dashboard"}
+						borderBottom
+						titleLink={<HowToUseBtn />}
+						createOfferBtn={{
+							label: "CREATE NEW",
+							onClick: () => {},
+						}}
+					/>
+				</>
+			}
+		></PageWrapper>
+	)
 }
