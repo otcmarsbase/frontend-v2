8439907cefab264406b87563a10ecaf51f1f1b96
dmi <dmi.villione@gmail.com>
2023-06-09 13:56:52 +0300

    add DasboardTabs

---
+++ src/components/Dashboard/Tabs.tsx
@@ -0,0 +1,10 @@
+import { Flex } from "@chakra-ui/react"
+import React from "react"
+
+type DasboardTabsProps = {
+	name: "offers" | "bids"
+}
+
+export const DasboardTabs: React.FC<DasboardTabsProps> = (props) => {
+	return <Flex></Flex>
+}
