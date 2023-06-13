19b88bf330eecad76d50774426bfba5917f22b59
dmi <dmi.villione@gmail.com>
2023-06-13 13:02:13 +0300

    [InfoTooltip] add size prop

--- src/components/InfoTooltip/InfoTooltip.tsx
+++ src/components/InfoTooltip/InfoTooltip.tsx
@@ -3,8 +3,14 @@ import { InfoIcon } from "@/icons"
 import React from "react"
 
 type InfoTooltipProps = React.ComponentProps<typeof WithTooltip> &
-	React.ComponentProps<typeof InfoIcon>
+	React.ComponentProps<typeof InfoIcon> & {
+		size?: "s" | "m"
+	}
 
+const sizes = {
+	s: "10",
+	m: "14",
+}
 export const InfoTooltip: React.FC<InfoTooltipProps> = ({
 	infoText,
 	placement,
