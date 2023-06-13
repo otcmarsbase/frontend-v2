9b3c21666fc2cb8be564bd3e1115cca45c3b0aaa
dmi <dmi.villione@gmail.com>
2023-06-13 13:02:13 +0300

    [CreateBid] add page wrapper

--- src/screens/CreateBid.tsx
+++ src/screens/CreateBid.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import { CreateBidHydrator } from "@/components/CreateBid/CreateBidHydrator"
+import { InfoTooltip } from "@/components/InfoTooltip/InfoTooltip"
+import { OfferTypeIndicator } from "@/components/OfferTypeIndicator/OfferTypeIndicator"
+import { PageHeader } from "@/components/PageHeader/PageHeader"
+import { PageWrapper } from "@/components/PageWrapper/PageWrapper"
+import { WithTooltip } from "@/components/WithTooltip/WithTooltip"
+import { Box, Flex } from "@chakra-ui/react"
+import React from "react"
 
 type CreateBidProps = {}
 
 export const CreateBid: React.FC<CreateBidProps> = ({}) => {
-    return <div>CreateBid screen</div>
+	return (
+		<PageWrapper
+			header={
+				<>
+					<PageHeader
+						subTitle={""}
+						title={"Offer Details"}
+						borderBottom
+						
+					>
+						<Box overflow={"scroll"} w={"full"}>
+							<Flex alignItems={"stretch"} w="full"></Flex>
+						</Box>
+					</PageHeader>
+				</>
+			}
+		>
+			<div className="max-w-[790px] mx-auto w-full pt-12 lg:pt-16">
+				<CreateBidHydrator />
+			</div>
+		</PageWrapper>
+	)
 }
