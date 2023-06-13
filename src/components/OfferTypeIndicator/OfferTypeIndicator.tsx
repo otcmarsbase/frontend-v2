aceb5a2b1a7f61251e8a5d302a4645cbc2265a8c
dmi <dmi.villione@gmail.com>
2023-06-13 13:02:13 +0300

    [OfferTypeIndicatorView add size prop

--- src/components/OfferTypeIndicator/OfferTypeIndicator.tsx
+++ src/components/OfferTypeIndicator/OfferTypeIndicator.tsx
@@ -8,6 +8,7 @@ import React from "react"
 type OfferTypeIndicatorProps = {
 	type: "static" | "dynamic"
 	text: string
+	size?: "s" | "m"
 }
 
 export const OfferTypeIndicatorView: React.FC<OfferTypeIndicatorProps> = ({
