c0e5866d0e225237051712c4c5cdce00547b3c66
dmi <dmi.villione@gmail.com>
2023-06-15 10:15:16 +0300

    add OfferInfoCard

---
+++ src/components/CreateBid/OfferInfoCard.tsx
@@ -0,0 +1,10 @@
+import { Flex } from "@chakra-ui/react"
+import React from "react"
+
+type CreateBidOfferInfoCardProps = {}
+
+export const CreateBidOfferInfoCard: React.FC<CreateBidOfferInfoCardProps> = (
+	props
+) => {
+	return <Flex></Flex>
+}
