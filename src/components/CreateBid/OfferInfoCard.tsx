ee0a071507d429f8750a2ef782b6964f6731a2aa
dmi <dmi.villione@gmail.com>
2023-06-15 10:15:16 +0300

    add CreateBidOfferInfoCardProps

--- src/components/CreateBid/OfferInfoCard.tsx
+++ src/components/CreateBid/OfferInfoCard.tsx
@@ -1,7 +1,13 @@
 import { Flex } from "@chakra-ui/react"
 import React from "react"
 
-type CreateBidOfferInfoCardProps = {}
+type CreateBidOfferInfoCardProps = {
+	name: string
+	value: string
+	description: string
+	tooltipText: string
+	icon: string
+}
 
 export const CreateBidOfferInfoCard: React.FC<CreateBidOfferInfoCardProps> = (
 	props
