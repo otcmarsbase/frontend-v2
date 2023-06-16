369dbe936f1e51061ad52265677ec36b8df5ac34
dmi <dmi.villione@gmail.com>
2023-06-16 12:46:18 +0300

    add createBidForm

---
+++ src/components/CreateBid/Form.tsx
@@ -0,0 +1,13 @@
+import { StepDisplay } from "@/components/CreateOffer/CreateOfferDetails/CreateOfferDetails"
+import { FormControlHeader } from "@/components/FormControlHeader/FormControlHeader"
+import { HowToUseBtn } from "@/components/HowToUseBtn/HowToUseBtn"
+import { Flex } from "@chakra-ui/react"
+import React from "react"
+
+type CreateBidFormProps = {}
+
+export const CreateBidForm: React.FC<CreateBidFormProps> = (props) => {
+	return (
+		
+	)
+}
