25b27acea25499557890f0756c2636f30d2a24fe
dmi <dmi.villione@gmail.com>
2023-06-19 15:13:26 +0300

    add CreateBidInputTokenOutAmount

---
+++ src/components/CreateBid/InputTokenOutAmount.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+type CreateBidInputTokenOutAmountProps = {}
+
+export const CreateBidInputTokenOutAmount: React.FC<
+	CreateBidInputTokenOutAmountProps
+> = (props) => {
+	return <div></div>
+}
