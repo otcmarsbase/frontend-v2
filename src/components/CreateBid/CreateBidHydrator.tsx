3d3dac6b5b61d467a2b16d6282d9f790010175ff
dmi <dmi.villione@gmail.com>
2023-06-19 15:13:26 +0300

    [CreateBidHydrator] add form

--- src/components/CreateBid/CreateBidHydrator.tsx
+++ src/components/CreateBid/CreateBidHydrator.tsx
@@ -1,7 +1,16 @@
+import { CreateBidForm } from "@/components/CreateBid/Form"
+import { FormControlHeader } from "@/components/FormControlHeader/FormControlHeader"
+import { HowToUseBtn } from "@/components/HowToUseBtn/HowToUseBtn"
+import { Flex } from "@chakra-ui/react"
 import React from "react"
 
 type CreateBidHydratorProps = {}
 
 export const CreateBidHydrator: React.FCC<CreateBidHydratorProps> = (props) => {
-	return <div></div>
+	
+	return (
+		<Flex>
+			<CreateBidForm />
+		</Flex>
+	)
 }
