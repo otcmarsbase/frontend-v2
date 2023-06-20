13919bebb56306b997deaa22a867f8d28a003c9f
dmi <dmi.villione@gmail.com>
2023-06-20 11:55:03 +0300

    [CurrencyPanel] add gap

--- src/components/CurrencyPanel/CurrencyPanel.tsx
+++ src/components/CurrencyPanel/CurrencyPanel.tsx
@@ -11,12 +11,15 @@ type CurrencyPanelProps = {
 
 export const CurrencyPanel: React.FCC<CurrencyPanelProps> = (props) => {
 	return (
+		<Flex
 			bg={"dark.800"}
 			direction={"row"}
 			alignItems={"center"}
 			px={"16px"}
 			py={"12px"}
 			borderRadius={"8px"}
+		>
+			<Flex gap={"10px"}>
 				{props.icon && <CurrencyIcon imgSrc={props.icon} />}
 				{props.name && <LeadText>{props.name}</LeadText>}
 			</Flex>
