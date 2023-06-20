3c8ffa233a54f711b0e11b82c245a26a5e7bbd26
dmi <dmi.villione@gmail.com>
2023-06-20 11:55:03 +0300

    [InputRedText] add props

--- src/components/Input/redText.tsx
+++ src/components/Input/redText.tsx
@@ -1,8 +1,10 @@
-
+import { Clickable } from "@/components/Clickable/Clickable"
+import { Text12Normal } from "@/components/Text/Typography"
 import React from "react"
 
 type InputRedTextProps = {
-	
+	text: string
+	onClick: () => void
 }
 
 export const InputRedText: React.FC<InputRedTextProps> = (props) => {
