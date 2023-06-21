e13ab62db8d5abff39b4f7c12e256d360854dd50
dmi <dmi.villione@gmail.com>
2023-06-21 10:36:53 +0300

    [CreateBidInputTokenOutAmount] add props

--- src/components/CreateBid/InputTokenOutAmount.tsx
+++ src/components/CreateBid/InputTokenOutAmount.tsx
@@ -1,6 +1,13 @@
+import { Input } from "@/components/Input/Input"
+import { RedControlLabel } from "@/components/Input/controlLabel"
+import { InputRedText } from "@/components/Input/redText"
+import { Flex } from "@chakra-ui/react"
 import React from "react"
 
-type CreateBidInputTokenOutAmountProps = {}
+type CreateBidInputTokenOutAmountProps = {
+	value: string
+	onChange: (value: string) => void
+}
 
 export const CreateBidInputTokenOutAmount: React.FC<
 	CreateBidInputTokenOutAmountProps
