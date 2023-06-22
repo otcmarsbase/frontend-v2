ec6d839c5dad48b3848dd4a5292448e3f0dcd3ca
dmi <dmi.villione@gmail.com>
2023-06-22 10:52:23 +0300

    add tip

---
+++ src/components/Tip/Tip.tsx
@@ -0,0 +1,10 @@
+import { Flex, FlexProps } from "@chakra-ui/react"
+import React from "react"
+
+type TipProps = {
+
+}
+
+export const Tip: React.FCC<TipProps> = (props) => {
+	return <div></div>
+}
