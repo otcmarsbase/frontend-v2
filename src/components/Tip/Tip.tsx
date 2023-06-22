e8c15cdd16fbee9986c92531b277cd4bee4af80a
dmi <dmi.villione@gmail.com>
2023-06-22 10:52:23 +0300

    add tip props

--- src/components/Tip/Tip.tsx
+++ src/components/Tip/Tip.tsx
@@ -2,7 +2,7 @@ import { Flex, FlexProps } from "@chakra-ui/react"
 import React from "react"
 
 type TipProps = {
-
+	color?: Pick<FlexProps, "bg">
 }
 
 export const Tip: React.FCC<TipProps> = (props) => {
