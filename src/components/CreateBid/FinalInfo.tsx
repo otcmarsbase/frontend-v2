3e3f8caf2e9d7fca046b004ffce5e18ef7ff1ea3
dmi <dmi.villione@gmail.com>
2023-06-22 10:52:23 +0300

    [CreateBidFinalInfo] add styles

--- src/components/CreateBid/FinalInfo.tsx
+++ src/components/CreateBid/FinalInfo.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { FromCurrToCurr } from "@/components/FromCurrToCurr/FromCurrToCurr"
+import { LeadText } from "@/components/Text/Typography"
+import { Flex } from "@chakra-ui/react"
+import React from "react"
 
 type CreateBidFinalInfoProps = {}
 
-export const CreateBidFinalInfo: React.FC<CreateBidFinalInfoProps> = (props) => {
-    return <div></div>
+export const CreateBidFinalInfo: React.FC<CreateBidFinalInfoProps> = (
+	props
+) => {
+	return (
+		<Flex bg={"rgba(36, 37, 45, 0.3)"} borderRadius={"12px"} w={"50%"}>
+			
+		</Flex>
+	)
 }
