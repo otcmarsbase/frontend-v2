bdf62f938d9cecc70a224db771e60edf29968c18
Azat Akhmetshin <raakhme@gmail.com>
2023-08-20 00:05:19 +0300

    TASK-213 fix .env variable name, add TODO for onAppLoad

--- src/app/onAppLoad.ts
+++ src/app/onAppLoad.ts
@@ -1,4 +1,5 @@
 import { createAppManager } from './logic';
+import { authLocalStore, instanceStore } from './store';
 
 export async function onAppLoad() {
   const appManager = createAppManager();
@@ -8,5 +9,17 @@ export async function onAppLoad() {
     serviceManager: { backendApiService },
   } = appManager;
 
-  // Здесь первоначальная прогрузка приложения
+  if (authLocalStore.authToken) {
+    try {
+      // TODO: implement checking of authToken and getting current user
+      // await backendApiService.rpcClient.send(
+      //   backendApiService.siteApi.auth.checkToken({}),
+      // );
+      // instanceStore.user = await backendApiService.rpcClient.send(
+      //   backendApiService.siteApi.auth.account.current({}),
+      // );
+    } catch (err) {
+      authLocalStore.authToken = null;
+    }
+  }
 }
