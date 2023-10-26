2254b1f6aabf59b748678cb8359c680e22d2256a
Mr.DoSS <artfut10@gmail.com>
2023-10-26 13:12:19 +0300

    Fix re-assign label

--- src/app/components/Resources/Lot/LotWizard/review/InvestDocWithReassignField.tsx
+++ src/app/components/Resources/Lot/LotWizard/review/InvestDocWithReassignField.tsx
@@ -4,6 +4,6 @@ import { StepReviewField } from '../_atoms';
 import { LotCreateModel } from '../schema';
 
 export const InvestDocWithReassignField = {
-  renderTitle: () => <Text>With reassign</Text>,
+  renderTitle: () => <Text>With re-assign</Text>,
   renderValue: (model) => <Text>{model.INVEST_DOC_WITH_REASSIGN ? 'Yes' : 'No'}</Text>,
 } satisfies StepReviewField<LotCreateModel>;
