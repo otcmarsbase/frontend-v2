283f90fb1ea7f50e7263402e281b4517cd3ce14b
Mr.DoSS <artfut10@gmail.com>
2023-12-18 18:02:01 +0500

    feature/1206100418249590

--- src/shared/schema/desk-gateway/src/RPC/DTO/Deal/DealListMy.ts
+++ src/shared/schema/desk-gateway/src/RPC/DTO/Deal/DealListMy.ts
@@ -5,6 +5,8 @@ import { Resource } from '../../../Resource';
 export namespace DealListMy {
   export type Payload = PaginationPayload & {
     status?: Resource.Deal.Enums.DealStatus[];
+    bids?: Resource.Bid.BidKey['id'][];
+    lots?: Resource.Lot.LotKey['id'][];
   };
   export type Result = Pagination<Resource.Deal.Deal>;
 }
