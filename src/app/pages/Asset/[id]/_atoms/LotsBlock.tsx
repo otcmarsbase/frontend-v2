c8145315e882ec2da079bdbba6fb2300b546842b
Kirill Kontorovich <ishimitsu.kk@gmail.com>
2024-03-28 03:37:13 +0500

    fixes for 9 tasks

--- src/app/pages/Asset/[id]/_atoms/LotsBlock.tsx
+++ src/app/pages/Asset/[id]/_atoms/LotsBlock.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useToggle } from 'react-use';
 
 import { UILogic, useRpcSchemaQuery } from '@app/components';
