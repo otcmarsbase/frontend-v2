b6a8cdd4cd5423758ed6ea7e4321a96530b508c0
Artur Korotenko <artfut10@gmail.com>
2024-08-05 20:02:15 +0500

    fix: build

--- src/shared/schema/desk-gateway/src/RPC/index.ts
+++ src/shared/schema/desk-gateway/src/RPC/index.ts
@@ -3,30 +3,28 @@
 import { RpcApiMethod, RpcApiMethodName, RpcApiPayload, RpcApiResult } from '@packages/berish-rpc-client-schema';
 import { RpcSchemaClientCommon } from '@schema/core';
 
-import * as _DTO from './DTO';
+import * as DTO from './DTO';
 import { Schema as _Schema } from './schema';
 
-export namespace RPC {
-  export import DTO = _DTO;
+export { DTO };
 
-  export type Schema = _Schema;
+export type Schema = _Schema;
 
-  export type MethodName = RpcApiMethodName<Schema>;
-  export type Method<TPath extends MethodName> = RpcApiMethod<Schema, TPath>;
-  export type Payload<TPath extends MethodName> = RpcApiPayload<Schema, TPath>;
-  export type Result<TPath extends MethodName> = RpcApiResult<Schema, TPath>;
+export type MethodName = RpcApiMethodName<Schema>;
+export type Method<TPath extends MethodName> = RpcApiMethod<Schema, TPath>;
+export type Payload<TPath extends MethodName> = RpcApiPayload<Schema, TPath>;
+export type Result<TPath extends MethodName> = RpcApiResult<Schema, TPath>;
 
-  export function MethodName<TPath extends MethodName>(path: TPath): TPath {
-    return path;
-  }
-
-  export interface ClientMeta {
-    token?: string;
-  }
-
-  export interface ServerMeta {
-    authToken?: string;
-  }
-
-  export class Client extends RpcSchemaClientCommon<Schema, ClientMeta, ServerMeta> {}
+export function MethodName<TPath extends MethodName>(path: TPath): TPath {
+  return path;
 }
+
+export interface ClientMeta {
+  token?: string;
+}
+
+export interface ServerMeta {
+  authToken?: string;
+}
+
+export class Client extends RpcSchemaClientCommon<Schema, ClientMeta, ServerMeta> {}
